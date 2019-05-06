4748a504914b25ce2495538f762e22cb88cce95e
stcr <stcr@equinor.com>
2019-05-06 14:37:54 +0200

    fix code quality

--- webviz_subsurface/containers/_volumetrics.py
+++ webviz_subsurface/containers/_volumetrics.py
@@ -1,13 +1,5 @@
-from uuid import uuid4
-import os
-import numpy as np
 import pandas as pd
-import dash_html_components as html
-import dash_core_components as dcc
-import dash_table
-from dash.dependencies import Input, Output
-from webviz_config.common_cache import cache
-from webviz_config.webviz_store import webvizstore
+
 
 class Volumetrics:
     '''### Volumetrics
@@ -27,10 +19,10 @@ This container visualizes RMS in-place volumetrics results
         self.ensemble_paths = tuple(
             (ens,
              container_settings['scratch_ensembles'][ens])
-             for ens in ensembles)
+            for ens in ensembles)
 
         self.volume_dfs = pd.concat(
             [pd.read_csv(ensemble_path + volfile)
              for ensemble_path in self.ensemble_paths])
-        
+
         print(self.volume_dfs)
