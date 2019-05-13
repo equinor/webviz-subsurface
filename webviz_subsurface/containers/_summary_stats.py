719306b23b035ab80093f30fab58bdb32c0b8ade
Stefan Crummenerl (ST MSU GEO) <stcr@equinor.com>
2019-05-13 10:44:31 +0200

    plot all real-traces individual

--- webviz_subsurface/containers/_summary_stats.py
+++ webviz_subsurface/containers/_summary_stats.py
@@ -192,7 +192,6 @@ def render_realization_plot(ensemble_paths, sampling, column_keys, vector):
             }
             traces.append(trace)
 
-
     layout = {
         'hovermode': 'closest',
         'barmode': 'overlay',
