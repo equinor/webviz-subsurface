c6169c91247e273b5987014b0f1cc3d05712fee9
Hans Kallekleiv <hakal@statoil.com>
2019-05-28 17:57:59 +0200

    Removed comment string

--- webviz_subsurface/datainput/__init__.py
+++ webviz_subsurface/datainput/__init__.py
@@ -3,7 +3,6 @@
 Contains data processing functions used in the containers.
 Some of the scripts are dependent on FMU postprocessing scripts
 that will be made open source in the near future.
-```
 '''
 
 from ._history_match import extract_mismatch, scratch_ensemble
