579f9b5da86a99254a5f101523b6c1f017689aa7
Hans Kallekleiv <lathans@protonmail.com>
2019-05-29 17:34:20 +0200

    Removed test data and fixed dataframe filtering

--- webviz_subsurface/containers/_morris_plot.py
+++ webviz_subsurface/containers/_morris_plot.py
@@ -9,7 +9,7 @@ from webviz_config.common_cache import cache
 from webviz_subsurface_components import Morris
 
 class MorrisPlot:
-    '''### Subsurface map
+    '''### Morris
 
 This container visualizes the subsurface. Currently only supporting reservoir
 model grid maps. In addition to show a map, it can visualize the flow pattern
@@ -32,22 +32,8 @@ in the simulation output using streamlines.
         self.csv_file = csv_file
         self.morris_data = read_csv(self.csv_file)
         self.vector_names = self.morris_data['name'].unique()
-        self.output = [
-            {'mean': 0.0, 'max': 0.0, 'min': 0.0, 'time': '2000-01-01T00:00:00'}, 
-            {'mean': 1821300.2, 'max': 2022804.5, 'min': 900429.1, 'time': '2001-01-01T00:00:00'}, 
-            {'mean': 3595926.9, 'max': 5060090.5, 'min': 1161664.8, 'time': '2002-01-01T00:00:00'}, 
-            {'mean': 4919365.7, 'max': 7102369.0, 'min': 2150000.5, 'time': '2003-01-01T00:00:00'}]
-
-        self.parameters = [
-            {'main': [0.0, 1327720.5, 3439176.1, 5311292.8], 'name': 'FWL', 'interactions': [0.0, 1116199.0, 2541439.9, 2836076.4]},
-            {'main': [0.0, 844.65, 5093.1, 12363.55], 'name': 'MULTFLT_F1', 'interactions': [0.0, 1231.4, 4597.0, 13793.5]},
-            {'main': [0.0, 908911.5, 1506246.1, 2000438.5], 'name': 'RANGE_PAR', 'interactions': [0.0, 1396000.4, 1900671.3, 1933889.5]},
-            {'main': [0.0, 10.1, 7413.1, 322.3], 'name': 'MULTZ_MIDREEK', 'interactions': [0.0, 211.1, 3098.9, 5619.7]}, 
-            {'main': [0.0, 1010601.3, 1822840.3, 2869195.5], 'name': 'AZIMUTH', 'interactions': [0.0, 1262311.8, 1822908.7, 2833047.4]},
-            {'main': [0.0, 167888.5, 398770.5, 598481.5], 'name': 'MEANPERMMULT', 'interactions': [0.0, 114457.6, 180225.4, 201267.2]}]
-
-        self.parameter = 'FOPT'
         self.set_callbacks(app)
+
     @property
     def layout(self):
         return html.Div([
@@ -58,10 +44,7 @@ in the simulation output using streamlines.
                                   for i in list(self.vector_names)],
                          value=self.vector_names[0]),
                    Morris(
-                    id=self.graph_id, 
-                    output=self.output,
-                    parameters=self.parameters,
-                    parameter=self.parameter)
+                    id=self.graph_id)
                ])
 
     def add_webvizstore(self):
@@ -80,13 +63,15 @@ in the simulation output using streamlines.
             output = output.drop_duplicates()
             output = output.to_dict(orient='records')
             parameters = []
-            for name in df['name'].unique():
-                name_df = df[df['name'] == name]
-                parameters.append({
-                    'main': list(name_df['morris_main']),
-                    'name': str(name),
-                    'interactions': list(name_df['morris_interaction'])
-                    })
+
+            for name in self.morris_data['name'].unique():
+                if name != vector:
+                    name_df = self.morris_data[self.morris_data['name'] == name]
+                    parameters.append({
+                        'main': list(name_df['morris_main']),
+                        'name': str(name),
+                        'interactions': list(name_df['morris_interaction'])
+                        })
             return output, vector, parameters
 
 
