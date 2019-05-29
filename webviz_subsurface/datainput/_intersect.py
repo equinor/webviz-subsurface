c6b539ca1b1d5b3d42871f87f7c47dc6ea5c019f
Hans Kallekleiv <hakal@statoil.com>
2019-05-29 11:01:04 +0200

    Remove unneccessary lines

--- webviz_subsurface/datainput/_intersect.py
+++ webviz_subsurface/datainput/_intersect.py
@@ -12,11 +12,8 @@ def load_well(well_name):
 
 def load_surface(s_name, real_path, surface_cat):
     path = os.path.join(real_path, 'share/results/maps',)
-    s_path = f'{s_name}--{surface_cat}.gri'
-    s_path = os.path.join(path, s_path)
+    s_path = os.path.join(path, f'{s_name}--{surface_cat}.gri')
     return xtgeo.surface.RegularSurface(s_path)
-
-# @cache.memoize(timeout=cache.TIMEOUT)
 
 
 def get_wfence(well_name, extend=200, tvdmin=0):
@@ -26,9 +23,6 @@ def get_wfence(well_name, extend=200, tvdmin=0):
     df.columns = df.columns.astype(str)
     return df
 
-# @cache.memoize(timeout=cache.TIMEOUT)
-
 
 def get_hfence(well, surface):
-    xyfence = surface.get_fence(get_wfence(well).values.copy())
-    return xyfence
+    return surface.get_fence(get_wfence(well).values.copy())
