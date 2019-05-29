bb0dbbeaf4e9f5d32cf3ac982cb85e29b7aa8123
Hans Kallekleiv <hakal@statoil.com>
2019-05-29 13:29:56 +0200

    Structural uncertainty intersection viewer (#15)
    * Initial intersect contaner setup
    
    * linting
    
    * Linting
    
    * Moved ensemble dependency out of container. Cleaned up imports
    
    * Linting
    
    * Fixed erroneous rename in last commit
    
    * Removed unneccessary abbreviation of imported dependency
    
    * Added explanation of container arguments
    
    * Made plot colors a class constant. Made surface color lookup more robust
    
    * Removing unneccessary change in history
    
    * Remove unneccessary lines
    
    * Improved comments. Added unique ids to html elements
    
    * Catch and allow missing surfaces
    
    * Caching calcuation of well fences
    
    * Made well selector non-clearable
    
    * Linting
    
    * Replacing equality check with boolean check for empty sequence
    
    * Made style options class constants

--- webviz_subsurface/datainput/__init__.py
+++ webviz_subsurface/datainput/__init__.py
@@ -1,11 +1,14 @@
 '''### _Subsurface data input_
-
 Contains data processing functions used in the containers.
 Some of the scripts are dependent on FMU postprocessing scripts
 that will be made open source in the near future.
 '''
 
 from ._history_match import extract_mismatch, scratch_ensemble
+from ._intersect import load_surface, get_wfence, get_hfence
 
-
-__all__ = ['scratch_ensemble', 'extract_mismatch']
+__all__ = ['scratch_ensemble',
+           'extract_mismatch',
+           'load_surface',
+           'get_wfence',
+           'get_hfence']
