aa3850697ebe3d375207cb439f8e4f86060924fd
Hans Kallekleiv <hakal@statoil.com>
2019-06-03 13:00:58 +0200

    Added density plot

--- webviz_subsurface/containers/_parameter_distribution.py
+++ webviz_subsurface/containers/_parameter_distribution.py
@@ -27,17 +27,6 @@ and correlation between the parameters as a correlation matrix.
 * `ensemble`: Which ensemble in `container_settings` to visualize.
 * `title`: Optional title for the container.
 '''
-    LAYOUT_STYLE = {
-        'display': 'grid',
-        'grid-template-areas':
-            'matrix matrix controls controls'
-            'matrix matrix scatter scatter '
-            'p1 p2 scatter scatter'
-    }
-    GRAPH_STYPE = {
-        'display': 'grid',
-        'grid-template-columns': '1fr 1fr 1fr 1fr',
-    }
 
     def __init__(self, app, container_settings, ensembles,
                  title: str = 'Parameter Distribution'):
@@ -47,9 +36,7 @@ and correlation between the parameters as a correlation matrix.
                           for ens in ensembles}
         self.uid = f'{uuid4()}'
         self.p1_drop_id = f'p1-dropd-{self.uid}'
-        self.p1_graph = f'p1-graph-{self.uid}'
         self.p2_drop_id = f'p2-dropd-{self.uid}'
-        self.p2_graph = f'p2-graph-{self.uid}'
         self.scatter_id = f'scatter-id-{self.uid}'
         self.scatter_color_id = f'scatter-color-id-{self.uid}'
         self.scatter_size_id = f'scatter-size-id-{self.uid}'
@@ -57,11 +44,12 @@ and correlation between the parameters as a correlation matrix.
         self.ens_matrix_id = f'ens-matrix-id-{self.uid}'
         self.ens_p1_id = f'ens-p1-id-{self.uid}'
         self.ens_p2_id = f'ens-p2-id-{self.uid}'
+        self.density_id = f'density-id-{self.uid}'
 
         self.set_callbacks(app)
 
     @property
-    def parameter_columns(self):
+    def p_cols(self):
         dfs = [get_parameters(ens) for ens in self.ensembles.values()]
         return sorted(list(pd.concat(dfs).columns))
 
@@ -70,8 +58,8 @@ and correlation between the parameters as a correlation matrix.
         return dcc.Graph(
             id=self.matrix_id,
             clickData={'points': [
-                {'x': self.parameter_columns[0]},
-                {'y': self.parameter_columns[0]}]},
+                {'x': self.p_cols[0],
+                 'y': self.p_cols[0]}]},
             config={
                 'displaylogo': False,
                 'modeBarButtonsToRemove': ['sendDataToCloud']
@@ -80,7 +68,6 @@ and correlation between the parameters as a correlation matrix.
     @property
     def control_div(self):
         return html.Div(
-            className='controls',
             style={'padding-top': 10, 'padding-left': 100},
             children=[
                 html.Div(
@@ -95,80 +82,63 @@ and correlation between the parameters as a correlation matrix.
                         html.Label('Parameter 1', style={
                             'font-weight': 'bold'}),
                         html.Div(
-                            style={'padding-bottom': 20},
+                            style={'padding-bottom': 20, 'display': 'grid',
+                                   'grid-template-columns': '3fr 2fr'},
                             children=[
                                 dcc.Dropdown(
                                     id=self.p1_drop_id,
                                     options=[{'label': p, 'value': p}
-                                             for p in self.parameter_columns],
-                                    value=self.parameter_columns[0]),
+                                             for p in self.p_cols],
+                                    value=self.p_cols[0]),
                                 Widgets.dropdown_from_dict(
                                     self.ens_p1_id, self.ensembles),
                             ]),
                         html.Label('Parameter 2', style={
                             'font-weight': 'bold'}),
-                        html.Div(style={'padding-bottom': 20},
-                                 children=[
+                        html.Div(style={
+                            'padding-bottom': 20,
+                            'display': 'grid',
+                            'grid-template-columns': '3fr 2fr'},
+                            children=[
                             dcc.Dropdown(
                                 id=self.p2_drop_id,
                                 options=[{'label': p, 'value': p}
-                                         for p in self.parameter_columns],
-                                value=self.parameter_columns[0]),
+                                         for p in self.p_cols],
+                                value=self.p_cols[0]),
                             Widgets.dropdown_from_dict(
                                 self.ens_p2_id, self.ensembles)
                         ]),
-                        html.Label('Color scatter by', style={
-                            'font-weight': 'bold'}),
-                        html.Div(style={'padding-bottom': 20},
-                                 children=[
-                            dcc.Dropdown(
-                                id=self.scatter_color_id,
-                                options=[{'label': p, 'value': p}
-                                         for p in self.parameter_columns])
-                        ])
-                    ])
+                        html.Label('Color scatter by',
+                                   style={'font-weight': 'bold'}),
+                        dcc.Dropdown(
+                            id=self.scatter_color_id,
+                            options=[{'label': p, 'value': p}
+                                     for p in self.p_cols])]),
+                html.Label('Density Plot', style={
+                    'font-weight': 'bold'}),
+                dcc.Dropdown(
+                    id=self.density_id,
+                    options=[{'label': 'On', 'value': True},
+                             {'label': 'Off', 'value': False}],
+                    value=True)
             ])
 
     @property
     def layout(self):
         return html.Div(
-            className="grid_container",
             children=[
                 html.Div(
-                    className='matrix',
-                    children=[self.matrix_plot]),
-                self.control_div,
-                html.Div(
-                    className='p1',
+                    style={'display': 'grid',
+                           'grid-template-columns': '3fr 2fr'},
                     children=[
-                        dcc.Graph(
-                            id=self.p1_graph,
-                            config={
-                                'displaylogo': False,
-                                'modeBarButtonsToRemove': ['sendDataToCloud']
-                            }),
-                    ]),
-                html.Div(
-                    className='p2',
-                    children=[
-                        dcc.Graph(
-                            id=self.p2_graph,
-                            config={
-                                'displaylogo': False,
-                                'modeBarButtonsToRemove': ['sendDataToCloud']
-                            }),
-                    ]),
-                html.Div(
-                    className='scatter',
-                    children=[
-                        dcc.Graph(
-                            id=self.scatter_id,
-                            config={
-                                'displaylogo': False,
-                                'modeBarButtonsToRemove': ['sendDataToCloud']
-                            })
-                    ])
-
+                        html.Div(
+                            children=[self.matrix_plot]), self.control_div]),
+                dcc.Graph(
+                    id=self.scatter_id,
+                    config={
+                        'displaylogo': False,
+                        'modeBarButtonsToRemove': ['sendDataToCloud']
+                    }),
             ])
 
     def set_callbacks(self, app):
@@ -177,26 +147,15 @@ and correlation between the parameters as a correlation matrix.
         def update_matrix(ens):
             return render_matrix(ens)
 
-        @app.callback(Output(self.p1_graph, 'figure'),
-                      [Input(self.ens_p1_id, 'value'),
-                       Input(self.p1_drop_id, 'value')])
-        def update_p1(ens, p):
-            return render_histogram(ens, p)
-
-        @app.callback(Output(self.p2_graph, 'figure'),
-                      [Input(self.ens_p2_id, 'value'),
-                       Input(self.p2_drop_id, 'value')])
-        def update_p2(ens, p):
-            return render_histogram(ens, p)
-
         @app.callback(Output(self.scatter_id, 'figure'),
                       [Input(self.ens_p1_id, 'value'),
                        Input(self.p1_drop_id, 'value'),
                        Input(self.ens_p2_id, 'value'),
                        Input(self.p2_drop_id, 'value'),
-                       Input(self.scatter_color_id, 'value')])
-        def update_scatter(ens1, p1, ens2, p2, color):
-            return render_scatter(ens1, p1, ens2, p2, color)
+                       Input(self.scatter_color_id, 'value'),
+                       Input(self.density_id, 'value')])
+        def update_scatter(ens1, p1, ens2, p2, color, density):
+            return render_scatter(ens1, p1, ens2, p2, color, density)
 
         @app.callback([Output(self.p1_drop_id, 'value'),
                        Output(self.p2_drop_id, 'value'),
@@ -225,62 +184,101 @@ def get_parameters(ensemble_path) -> pd.DataFrame:
 
 
 @cache.memoize(timeout=cache.TIMEOUT)
-def render_scatter(ens1, x, ens2, y, color):
+def render_scatter(ens1, x_col, ens2, y_col, color, density):
     if ens1 == ens2:
         real_text = [f'Realization:{r}'
                      for r in get_parameters(ens1)['REAL']]
     else:
         real_text = [f'Realization:{r}(x)'
                      for r in get_parameters(ens2)['REAL']]
-    print(color)
-    data = {
-        'x': get_parameters(ens1)[x],
-        'y': get_parameters(ens2)[y],
+
+    x = get_parameters(ens1)[x_col]
+    y = get_parameters(ens2)[y_col]
+    color = get_parameters(ens1)[color] if color else None
+    data = []
+    data.append({
+        'x': x,
+        'y': y,
         'marker': {
-            'color': get_parameters(ens1)[color] if color else None
+            'color': color
         },
         'text': real_text,
         'type': 'scatter',
-        'mode': 'markers'
-    }
+        'mode': 'markers',
+        'showlegend': False
+    })
+    data.append({
+        'x': x,
+        'type': 'histogram',
+        'yaxis': 'y2',
+        'showlegend': False,
+        "marker": {"color": "rgb(31, 119, 180)"},
+    })
+    data.append({
+        'y': y,
+        'type': 'histogram',
+        'xaxis': 'x2',
+        'showlegend': False,
+        "marker": {"color": "rgb(31, 119, 180)"}
+    })
+    if density:
+        data.append({
+            'x': x,
+            'y': y,
+            "autocolorscale": False,
+            'showlegend': False,
+            "colorscale": [
+                [0, "rgb(255,255,255)"],
+                [0.125, "rgb(37, 52, 148)"],
+                [0.25, "rgb(34, 94, 168)"],
+                [0.375, "rgb(29, 145, 192)"],
+                [0.5, "rgb(65, 182, 196)"],
+                [0.625, "rgb(127, 205, 187)"],
+                [0.75, "rgb(199, 233, 180)"],
+                [0.875, "rgb(237, 248, 217)"],
+                [1, "rgb(255, 255, 217)"]],
+            "contours": {
+                "coloring": "fill",
+                # "end": 80.05,
+                "showlines": True,
+                "size": 5,
+                "start": 5
+            },
+            "name": "density",
+            "ncontours": 20,
+            "reversescale": False,
+            "showscale": False,
+            "type": "histogram2dcontour",
+        })
     layout = {
-        'margin': {'t': 50, 'b': 50},
-        'xaxis': {'title': x},
-        'yaxis': {'title': y},
-    }
-    return {'data': [data], 'layout': layout}
-
-
-@cache.memoize(timeout=cache.TIMEOUT)
-def render_bar(ens, y):
-    data = {
-        'y': get_parameters(ens)[y],
-        'x': [r for r in get_parameters(ens)['REAL']],
-        'type': 'bar',
-        # 'mode': 'markers'
-    }
-    layout = {
-        'margin': {'t': 50, 'b': 50},
-        'xaxis': {'title': 'Realization'},
-        'yaxis': {'title': y},
-    }
-    return {'data': [data], 'layout': layout}
-
-
-@cache.memoize(timeout=cache.TIMEOUT)
-def render_histogram(ensemble_path, parameter):
-    data = {
-        'x': get_parameters(ensemble_path)[parameter],
-        'type': 'histogram'
-    }
-
-    layout = {
-        'margin': {'t': 50, 'b': 50},
+        'margin': {'t': 20, 'b': 20, 'l': 200, 'r': 200},
         'bargap': 0.05,
-        'xaxis': {'title': parameter},
+        'xaxis': {
+            'title': x_col,
+            'domain': [0, 0.85],
+            'showgrid': 'false',
+            'zeroline': 'false',
+            'showlegend': False
+        },
+        'xaxis2': {
+            'domain': [0.85, 1],
+            'showgrid': 'false',
+            'zeroline': 'false'
+        },
+        'yaxis': {
+            'title': y_col,
+            'domain': [0, 0.85],
+            'showgrid': 'false',
+            'zeroline': 'false'
+        },
+        'yaxis2': {
+            'domain': [0.85, 1],
+            'showgrid': 'false',
+            'zeroline': 'false'
+        }
     }
 
-    return {'data': [data], 'layout': layout}
+    return {'data': data, 'layout': layout}
 
 
 @cache.memoize(timeout=cache.TIMEOUT)
@@ -293,11 +291,13 @@ def render_matrix(ensemble_path):
         'type': 'heatmap',
         'x': data.columns,
         'y': data.columns,
-        'z': values
+        'z': values,
+        'zmin': -1,
+        'zmax': 1
     }
 
     layout = {
-        'title': 'Correlation matrix',
+        'title': 'Pairwise correlation matrix',
         'margin': {'t': 50, 'b': 50},
         'xaxis': {'showticklabels': False},
         'yaxis': {'showticklabels': False},
