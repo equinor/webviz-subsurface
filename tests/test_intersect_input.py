bfae17326c79192f8456db2dd29ccea0d03f06d2
Hans Kallekleiv <hakal@statoil.com>
2019-06-20 16:15:46 +0200

    Cleanup test

--- tests/test_intersect_input.py
+++ tests/test_intersect_input.py
@@ -1,22 +1,11 @@
-
+import webviz_subsurface.datainput._intersect as intersect
 from mock import patch
 from functools import wraps
 
 
-def mock_decorator(*args, **kwargs):
-    def decorator(f):
-        @wraps(f)
-        def decorated_function(*args, **kwargs):
-            return f(*args, **kwargs)
-        return decorated_function
-    return decorator
 
 patch('webviz_config.common_cache.cache.memoize', lambda *x, **y: lambda f: f).start()
 patch('webviz_config.webviz_store.webvizstore', lambda *x, **y: lambda f: f).start()
-import webviz_subsurface.datainput._intersect as intersect
-def test_get_wfence():
-	assert intersect.get_wfence('/scratch/fmu/hakal/reek_wells/OP_1.rmswell')[0].mean() == 0
-
 def test_surface_to_df():
     df = intersect.surface_to_df('base_hugin',
                                  '/scratch/fmu/hakal/volve/volve_example3/realization-0/iter-0',
@@ -31,3 +20,6 @@ def test_surface_to_df():
                        'rotation',
                        'values']
 
+
+def test_get_wfence():
+	assert intersect.get_wfence('/scratch/fmu/hakal/reek_wells/OP_1.rmswell')[0].mean() == 0
