3b3c6f4e5736d9edd7b0178d6b1abcbedece0817
Hans Kallekleiv <hakal@statoil.com>
2019-08-07 11:30:34 +0200

    Use pandas isnumeric function instead of checking for np.number

--- webviz_subsurface/containers/_parameter_distribution.py
+++ webviz_subsurface/containers/_parameter_distribution.py
@@ -1,5 +1,4 @@
 from uuid import uuid4
-import numpy as np
 import pandas as pd
 import dash_html_components as html
 import dash_core_components as dcc
@@ -320,7 +319,8 @@ def render_scatter(ens1, x_col, ens2, y_col, color, density):
 @cache.memoize(timeout=cache.TIMEOUT)
 def render_matrix(ensemble_path):
 
-    data = get_parameters(ensemble_path).select_dtypes([np.number])
+    data = get_parameters(ensemble_path).apply(pd.to_numeric, errors='coerce')\
+                                        .dropna(how='all', axis='columns')
     values = list(data.corr().values)
 
     data = {
