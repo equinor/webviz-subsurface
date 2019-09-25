ca3d89afe33b30e13f18f918343661ea8f4ec2df
Hans Kallekleiv <hakal@statoil.com>
2019-09-25 14:46:28 +0200

    Added function to find surfaces on disk

--- webviz_subsurface/datainput/_fmu_input.py
+++ webviz_subsurface/datainput/_fmu_input.py
@@ -1,3 +1,5 @@
+import os
+import glob
 import pandas as pd
 
 try:
@@ -38,3 +40,51 @@ def get_realizations(
         lambda x: ens_set[x["ENSEMBLE"]][x["REAL"]].runpath(), axis=1
     )
     return df.sort_values(by=["ENSEMBLE", "REAL"])
+
+
+@cache.memoize(timeout=cache.TIMEOUT)
+def find_surfaces(ensemble_paths: tuple, suffix="*.gri"):
+    """Reads surface file names stored in standard FMU format, and returns a dictionary
+    on the following format:
+    surface_property:
+        names:
+            - some_surface_name
+            - another_surface_name
+        dates:
+            - some_date
+            - another_date
+    """
+
+    files = []
+    for name, path in ensemble_paths:
+        files.extend(glob.glob(os.path.join(path, "share", "results", "maps", suffix)))
+
+    files = [os.path.basename(f.split(".gri")[0]) for f in files]
+    files = [f.split("--") for f in files]
+    arr = []
+    for f in files:
+        data = {}
+        try:
+            data["attribute"] = f[1]
+        except IndexError:
+            continue
+        try:
+            data["name"] = f[0]
+        except IndexError:
+            data["name"] = None
+        try:
+            data["date"] = f[2]
+        except IndexError:
+            data["date"] = None
+        arr.append(data)
+
+    df = pd.DataFrame(arr)
+
+    context = {}
+    for attr, dframe in df.groupby("attribute"):
+        context[attr] = {
+            "names": list(dframe["name"].unique()),
+            "dates": list(dframe["date"].unique()),
+        }
+
+    return context
