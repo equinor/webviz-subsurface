2a574eb7d5b8a8d1b509d8c2f1d7477a6c1c2a85
Hans Kallekleiv <lathans@protonmail.com>
2019-09-26 15:36:26 +0200

    Refactor find_surfaces function

--- webviz_subsurface/datainput/_fmu_input.py
+++ webviz_subsurface/datainput/_fmu_input.py
@@ -1,4 +1,5 @@
 import os
+from pathlib import Path
 import glob
 import pandas as pd
 
@@ -11,10 +12,15 @@ from webviz_config.webviz_store import webvizstore
 
 
 @cache.memoize(timeout=cache.TIMEOUT)
-def load_ensemble_set(ensemble_paths: tuple, ensemble_set_name: str = "EnsembleSet"):
+def load_ensemble_set(
+    ensemble_paths: tuple, ensemble_set_name: str = "EnsembleSet"
+):
     return EnsembleSet(
         ensemble_set_name,
-        [ScratchEnsemble(ens_name, ens_path) for ens_name, ens_path in ensemble_paths],
+        [
+            ScratchEnsemble(ens_name, ens_path)
+            for ens_name, ens_path in ensemble_paths
+        ],
     )
 
 
@@ -45,14 +51,15 @@ def get_realizations(
     df["SENSCASE"] = ens_set.parameters.get("SENSCASE")
     df["SENSNAME"] = ens_set.parameters.get("SENSNAME")
     df["RUNPATH"] = df.apply(
-        #Extracts realization runpath from the EnsembleSet.ScratchEnsemble.Realization object
-        lambda x: ens_set[x["ENSEMBLE"]][x["REAL"]].runpath(), axis=1
+        # Extracts realization runpath from the EnsembleSet.ScratchEnsemble.Realization object
+        lambda x: ens_set[x["ENSEMBLE"]][x["REAL"]].runpath(),
+        axis=1,
     )
     return df.sort_values(by=["ENSEMBLE", "REAL"])
 
 
 @cache.memoize(timeout=cache.TIMEOUT)
-def find_surfaces(ensemble_paths: tuple, suffix="*.gri"):
+def find_surfaces(ensemble_paths: tuple, suffix="*.gri", delimiter="--"):
     """Reads surface file names stored in standard FMU format, and returns a dictionary
     on the following format:
     surface_property:
@@ -63,32 +70,19 @@ def find_surfaces(ensemble_paths: tuple, suffix="*.gri"):
             - some_date
             - another_date
     """
-
+    # Create list of all files in all realizations in all ensembles
     files = []
     for _, path in ensemble_paths:
-        files.extend(glob.glob(os.path.join(path, "share", "results", "maps", suffix)))
+        path = Path(path)
+        files += glob.glob(str(path / "share" / "results" / "maps" / suffix))
 
-    files = [os.path.basename(f.split(".gri")[0]) for f in files]
-    files = [f.split("--") for f in files]
-    arr = []
-    for f in files:
-        data = {}
-        try:
-            data["attribute"] = f[1]
-        except IndexError:
-            continue
-        try:
-            data["name"] = f[0]
-        except IndexError:
-            data["name"] = None
-        try:
-            data["date"] = f[2]
-        except IndexError:
-            data["date"] = None
-        arr.append(data)
+    # Store surface name, attribute and date as Pandas dataframe
+    df = pd.DataFrame(
+        [Path(f).stem.split(delimiter) for f in files],
+        columns=["name", "attribute", "date"],
+    )
 
-    df = pd.DataFrame(arr)
-
+    # Group dataframe by surface attribute and return unique names and dates
     context = {}
     for attr, dframe in df.groupby("attribute"):
         context[attr] = {
@@ -96,4 +90,10 @@ def find_surfaces(ensemble_paths: tuple, suffix="*.gri"):
             "dates": list(dframe["date"].unique()),
         }
 
-    return context
+    return {
+        attr: {
+            "names": list(dframe["name"].unique()),
+            "dates": list(dframe["date"].unique()),
+        }
+        for attr, dframe in df.groupby("attribute")
+    }
