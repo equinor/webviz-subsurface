d91bb632b791f3f3209c8b48ceb3770b1f10c492
Hans Kallekleiv <lathans@protonmail.com>
2019-09-29 13:44:23 +0200

    Added callback for hover and click data

--- webviz_subsurface/private_containers/_tornado_plot.py
+++ webviz_subsurface/private_containers/_tornado_plot.py
@@ -31,16 +31,27 @@ The format of the json dump must be:
 
 """
 
-    def __init__(self, app, realizations, reference="rms_seed"):
+    def __init__(
+        self,
+        app,
+        realizations,
+        reference="rms_seed",
+        allow_hover=False,
+        allow_click=False,
+    ):
 
         self.realizations = realizations
         self.senscases = list(self.realizations["SENSNAME"].unique())
         self.initial_reference = (
             reference if reference in self.senscases else self.senscases[0]
         )
+        self.allow_hover = allow_hover
+        self.allow_click = allow_click
         self._storage_id = f"{str(uuid4())}-tornado-data"
         self._reference_id = f"{str(uuid4())}-reference"
         self._graph_id = f"{str(uuid4())}-graph"
+        self._hover_id = f"{str(uuid4())}-hover"
+        self._click_id = f"{str(uuid4())}-click"
         self._scale_id = f"{str(uuid4())}-scale"
         self.set_callbacks(app)
 
@@ -48,6 +59,16 @@ The format of the json dump must be:
     def storage_id(self):
         """The id of the dcc.Store component that holds the tornado data"""
         return self._storage_id
+
+    @property
+    def hover_id(self):
+        """The id of the dcc.Store component that holds hover data"""
+        return self._hover_id
+
+    @property
+    def click_id(self):
+        """The id of the dcc.Store component that holds click data"""
+        return self._click_id
 
     @property
     def layout(self):
@@ -68,7 +89,9 @@ The format of the json dump must be:
                     ],
                     value="Percentage",
                 ),
-                wcc.Graph(id=self._graph_id),
+                wcc.Graph(id=self._graph_id, config={"displayModeBar": False}),
+                dcc.Store(id=self.hover_id),
+                dcc.Store(id=self.click_id),
             ]
         )
 
@@ -82,6 +105,8 @@ The format of the json dump must be:
             ],
         )
         def _calc_tornado(reference, scale, data):
+            if not data:
+                raise PreventUpdate
             data = json.loads(data)
             if not isinstance(data, dict):
                 raise PreventUpdate
@@ -89,13 +114,44 @@ The format of the json dump must be:
             realizations = self.realizations.loc[
                 self.realizations["ENSEMBLE"] == data["ENSEMBLE"]
             ]
-            return tornado_plot(realizations, values, reference, scale)
+            try:
+                return tornado_plot(realizations, values, reference, scale)
+            except KeyError:
+                return {}
+
+        if self.allow_hover:
+
+            @app.callback(
+                Output(self.hover_id, "children"), [Input(self._graph_id, "hoverData")]
+            )
+            def _save_hover_data(data):
+                try:
+                    real_low = data["points"][0]["customdata"]
+                    real_high = data["points"][1]["customdata"]
+                    return json.dumps({"real_low": real_low, "real_high": real_high})
+                except TypeError:
+                    raise PreventUpdate
+
+        if self.allow_hover:
+
+            @app.callback(
+                Output(self.click_id, "children"), [Input(self._graph_id, "clickData")]
+            )
+            def _save_hover_data(data):
+                try:
+                    real_low = data["points"][0]["customdata"]
+                    real_high = data["points"][1]["customdata"]
+                    return json.dumps({"real_low": real_low, "real_high": real_high})
+                except TypeError:
+                    raise PreventUpdate
 
 
 def tornado_plot(realizations, data, reference="rms_seed", scale="Percentage"):
 
+    # Raise key error if no senscases, i.e. the ensemble has no design matrix
     if list(realizations["SENSCASE"].unique()) == [None]:
-        return {}
+        raise KeyError
+
     # Calculate average response value for reference sensitivity
     ref_avg = data.loc[
         data["REAL"].isin(
@@ -103,8 +159,8 @@ def tornado_plot(realizations, data, reference="rms_seed", scale="Percentage"):
         )
     ]["VALUE"].mean()
 
-    arr = []
     # Group by sensitivity name/case and calculate average values for each case
+    arr = []
     for (sens_name, sens_case), dframe in realizations.groupby(
         ["SENSNAME", "SENSCASE"]
     ):
@@ -125,11 +181,10 @@ def tornado_plot(realizations, data, reference="rms_seed", scale="Percentage"):
             }
         )
 
-    arr2 = []
     # Group by sensitivity name and calculate low / high values
+    arr2 = []
     for sensname, dframe in pd.DataFrame(arr).groupby(["sensname"]):
         low = dframe.loc[dframe["values_ref"].idxmin()]
-
         high = dframe.loc[dframe["values_ref"].idxmax()]
 
         arr2.append(
@@ -137,12 +192,12 @@ def tornado_plot(realizations, data, reference="rms_seed", scale="Percentage"):
                 "low": low["values_ref"] if low["values_ref"] < 0 else 0,
                 "low_label": low["senscase"],
                 "true_low": low["values"],
-                "low_reals": low["reals"] if low["values_ref"] < 0 else [],
+                "low_reals": low["reals"],
                 "name": sensname,
                 "high": high["values_ref"] if high["values_ref"] > 0 else 0,
                 "high_label": high["senscase"],
                 "true_high": high["values"],
-                "high_reals": high["reals"] if high["values_ref"] > 0 else [],
+                "high_reals": high["reals"],
             }
         )
     df = pd.DataFrame(arr2).sort_values(by=["high"])
@@ -155,6 +210,7 @@ def tornado_plot(realizations, data, reference="rms_seed", scale="Percentage"):
                 y=df["name"],
                 x=df["low"],
                 name="low",
+                customdata=df["low_reals"],
                 hovertext=[
                     f"Case: {label}<br>True Value: {val:.2f}<br>Realizations:"
                     f"{min(reals) if reals else None}-{max(reals) if reals else None}"
@@ -171,6 +227,7 @@ def tornado_plot(realizations, data, reference="rms_seed", scale="Percentage"):
                 y=df["name"],
                 x=df["high"],
                 name="high",
+                customdata=df["high_reals"],
                 hovertext=[
                     f"Case: {label}<br>True Value: {val:.2f}<br>Realizations:"
                     f"{min(reals) if reals else None}-{max(reals) if reals else None}"
@@ -183,5 +240,21 @@ def tornado_plot(realizations, data, reference="rms_seed", scale="Percentage"):
                 marker=dict(color="rgb(36, 55, 70)"),
             ),
         ],
-        "layout": {"barmode": "relative"},
+        "layout": {
+            "barmode": "relative",
+            "margin": {"l": 50, "r": 50, "b": 20, "t": 50},
+            "xaxis": {
+                "autorange": True,
+                "showgrid": False,
+                "zeroline": False,
+                "showline": False,
+            },
+            "yaxis": {
+                "autorange": True,
+                "showgrid": False,
+                "zeroline": False,
+                "showline": False,
+            },
+            "showlegend": False,
+        },
     }
