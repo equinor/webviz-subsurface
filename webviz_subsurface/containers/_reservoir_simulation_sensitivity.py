32e2a7d8b660b4607b4f1e79c3203ae0dd52d915
Hans Kallekleiv <hakal@statoil.com>
2019-10-01 13:58:49 +0200

    Remove double space

--- webviz_subsurface/containers/_reservoir_simulation_sensitivity.py
+++ webviz_subsurface/containers/_reservoir_simulation_sensitivity.py
@@ -19,7 +19,7 @@ from ..datainput import load_smry, get_realizations
 
 
 class ReservoirSimulationSensitivity(WebvizContainer):
-    """### Visualizes reservoir simulation time series  related to a sensitivity run
+    """### Visualizes reservoir simulation time series related to a sensitivity run
 
 * `ensembles`: Which ensembles in `container_settings` to visualize.
 * `column_keys`: List of vectors to extract. If not given, all vectors
