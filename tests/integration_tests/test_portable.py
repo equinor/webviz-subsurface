fb1d6ee23014adc316515bbddd3729d4c7f2a608
Hans Kallekleiv <lathans@protonmail.com>
2019-11-13 15:43:22 +0100

    added test

---
+++ tests/integration_tests/test_portable.py
@@ -0,0 +1,25 @@
+import sys
+import subprocess
+
+
+def test_portable(dash_duo, tmp_path):
+    # Build a portable webviz from config file
+    appdir = tmp_path / 'app'
+    subprocess.call(['webviz', 'build', 'reek_example.yaml',
+                     '--portable', appdir], cwd='examples')
+    # Remove Talisman
+    fn = appdir / 'webviz_app.py'
+    with open(fn, "r") as f:
+        lines = f.readlines()
+    with open(fn, "w") as f:
+        for line in lines:
+            if not line.strip("\n").startswith("Talisman"):
+                f.write(line)
+    # Import generated app
+    sys.path.append(str(appdir))
+    from webviz_app import app
+    # Start and test app
+    dash_duo.start_server(app)
+    for page in ['inplace_volume_sensitivities', 'last_page']:
+        dash_duo.wait_for_element(f'#{page}').click()
+    assert dash_duo.get_logs() == [], 'browser console should contain no error'
