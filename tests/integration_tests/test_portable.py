e5652d30b341220b92306a2f558b6c72a8b79801
Hans Kallekleiv <lathans@protonmail.com>
2019-11-13 15:53:07 +0100

    linting. added test dependencies

--- tests/integration_tests/test_portable.py
+++ tests/integration_tests/test_portable.py
@@ -4,11 +4,12 @@ import subprocess
 
 def test_portable(dash_duo, tmp_path):
     # Build a portable webviz from config file
-    appdir = tmp_path / 'app'
-    subprocess.call(['webviz', 'build', 'reek_example.yaml',
-                     '--portable', appdir], cwd='examples')
+    appdir = tmp_path / "app"
+    subprocess.call(
+        ["webviz", "build", "reek_example.yaml", "--portable", appdir], cwd="examples"
+    )
     # Remove Talisman
-    fn = appdir / 'webviz_app.py'
+    fn = appdir / "webviz_app.py"
     with open(fn, "r") as f:
         lines = f.readlines()
     with open(fn, "w") as f:
@@ -18,8 +19,9 @@ def test_portable(dash_duo, tmp_path):
     # Import generated app
     sys.path.append(str(appdir))
     from webviz_app import app
+
     # Start and test app
     dash_duo.start_server(app)
-    for page in ['inplace_volume_sensitivities', 'last_page']:
-        dash_duo.wait_for_element(f'#{page}').click()
-    assert dash_duo.get_logs() == [], 'browser console should contain no error'
+    for page in ["inplace_volume_sensitivities", "last_page"]:
+        dash_duo.wait_for_element(f"#{page}").click()
+    assert dash_duo.get_logs() == [], "browser console should contain no error"
