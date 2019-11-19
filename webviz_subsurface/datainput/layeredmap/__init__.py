fedb7aa318ba2ad97afc4bb82e000c50ed92130b
Hans Kallekleiv <hakal@statoil.com>
2019-11-19 11:33:24 +0100

    Remove layered map input functions (#135)

--- webviz_subsurface/datainput/layeredmap/__init__.py
+++
@@ -1,9 +0,0 @@
-"""### _Subsurface data input_
-Contains data processing functions to generate input for the LayeredMap
-visualization in webviz-subsurface-components.
-"""
-
-from ._layered_surface import LayeredSurface
-from ._layered_fence import LayeredFence
-
-__all__ = ["LayeredSurface", "LayeredFence"]
