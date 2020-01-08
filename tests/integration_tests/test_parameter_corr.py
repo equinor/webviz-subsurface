223c338b59fa8578423376bf2b3e6da6f93e9b3a
Hans Kallekleiv <hakal@statoil.com>
2020-01-08 13:49:19 +0100

    Update test

--- tests/integration_tests/test_parameter_corr.py
+++ tests/integration_tests/test_parameter_corr.py
@@ -2,6 +2,7 @@ import mock
 import dash
 import pandas as pd
 from webviz_config.common_cache import CACHE
+from webviz_config.themes import default_theme
 from webviz_config.plugins import ParameterCorrelation
 
 # mocked functions
@@ -15,7 +16,10 @@ def test_parameter_corr(dash_duo):
     app.scripts.config.serve_locally = True
     app.config.suppress_callback_exceptions = True
     CACHE.init_app(app.server)
-    app.webviz_settings = {"shared_settings": {"scratch_ensembles": {"iter-0": ""}}}
+    app.webviz_settings = {
+        "shared_settings": {"scratch_ensembles": {"iter-0": ""}},
+        "theme": default_theme,
+    }
     ensembles = ["iter-0"]
 
     with mock.patch(get_parameters) as mock_parameters:
