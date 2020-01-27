32ccbb7fd846e980466671509bff85f51488749f
Hans Kallekleiv <hakal@statoil.com>
2020-01-27 13:40:17 +0100

    Small fixes to surface calculations (#207)

--- webviz_subsurface/plugins/_well_cross_section_fmu.py
+++ webviz_subsurface/plugins/_well_cross_section_fmu.py
@@ -556,7 +556,6 @@ def calculate_surface_statistics(
         for real_path in real_paths
     ]
     surfaces = get_surfaces(fns)
-
     return io.BytesIO(
         json.dumps(
             {
@@ -591,7 +590,7 @@ def surface_to_json(surface):
             "rotation": surface.rotation,
             "xinc": surface.xinc,
             "yinc": surface.yinc,
-            "values": surface.values.tolist(),
+            "values": surface.values.copy().filled(np.nan).tolist(),
         }
     )
 
