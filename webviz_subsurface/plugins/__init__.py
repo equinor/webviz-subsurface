f8950ac6a78db5be1d9a9156bc8604a6ee29b8e6
Asgeir Nyvoll <asny@equinor.com>
2020-03-30 15:49:46 +0200

    relperm

--- webviz_subsurface/plugins/__init__.py
+++ webviz_subsurface/plugins/__init__.py
@@ -44,6 +44,7 @@ from ._well_cross_section import WellCrossSection
 from ._well_cross_section_fmu import WellCrossSectionFMU
 from ._parameter_parallel_coordinates import ParameterParallelCoordinates
 from ._running_time_analysis_fmu import RunningTimeAnalysisFMU
+from ._relative_permeability import RelativePermeability
 
 
 __all__ = [
@@ -64,4 +65,5 @@ __all__ = [
     "WellCrossSection",
     "ParameterParallelCoordinates",
     "RunningTimeAnalysisFMU",
+    "RelativePermeability",
 ]
