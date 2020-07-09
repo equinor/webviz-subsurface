44f4f9804c25b8c95f6db1fb19d9a7fe74934547
Sofie Aasheim <sofieaaf@gmail.com>
2020-07-09 16:03:19 +0200

    Add plugin for correlations with interaction terms

---
+++ webviz_subsurface/plugins/_correlation_interactions.py
@@ -0,0 +1,484 @@
+from uuid import uuid4
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+
+from scipy.stats import pearsonr, spearmanr
+import itertools
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+from dash.exceptions import PreventUpdate
+from dash_table import DataTable
+from dash.dependencies import Input, Output
+import dash_html_components as html
+import dash_core_components as dcc
+from dash_table.Format import Format, Scheme
+import webviz_core_components as wcc
+from webviz_config.webviz_store import webvizstore
+from webviz_config.common_cache import CACHE
+from webviz_config import WebvizPluginABC
+from webviz_config.utils import calculate_slider_step
+import statsmodels.formula.api as smf
+import statsmodels.api as sm
+from sklearn.preprocessing import PolynomialFeatures
+
+from .._datainput.fmu_input import load_parameters, load_csv
+
+class CorrelationInteractions(WebvizPluginABC):
+    # pylint:disable=too-many-arguments
+    def __init__(
+        self,
+        app,
+        parameter_csv: Path = None,
+        response_csv: Path = None,
+        ensembles: list = None,
+        response_file: str = None,
+        response_filters: dict = None,
+        response_ignore: list = None,
+        response_include: list = None,
+        column_keys: list = None,
+        sampling: str = "monthly",
+        aggregation: str = "sum",
+    ):
+
+        super().__init__()
+
+        self.parameter_csv = parameter_csv if parameter_csv else None
+        self.response_csv = response_csv if response_csv else None
+        self.response_file = response_file if response_file else None
+        self.response_filters = response_filters if response_filters else {}
+        self.response_ignore = response_ignore if response_ignore else None
+        self.column_keys = column_keys
+        self.time_index = sampling
+        self.aggregation = aggregation
+
+        if response_ignore and response_include:
+            raise ValueError(
+                'Incorrent argument. either provide "response_include", '
+                '"response_ignore" or neither'
+            )
+        if parameter_csv and response_csv:
+            if ensembles or response_file:
+                raise ValueError(
+                    'Incorrect arguments. Either provide "csv files" or '
+                    '"ensembles and response_file".'
+                )
+            #For csv files
+            self.parameterdf = read_csv(self.parameter_csv)
+            self.responsedf = read_csv(self.response_csv)
+
+            #For parquet files
+            #self.parameterdf = pd.read_parquet(self.parameter_csv)
+            #self.responsedf = pd.read_parquet(self.response_csv)
+
+        elif ensembles and response_file:
+            self.ens_paths = {
+                ens: app.webviz_settings["shared_settings"]["scratch_ensembles"][ens]
+                for ens in ensembles
+            }
+            self.parameterdf = load_parameters(
+                ensemble_paths=self.ens_paths, ensemble_set_name="EnsembleSet"
+            )
+            self.responsedf = load_csv(
+                ensemble_paths=self.ens_paths,
+                csv_file=response_file,
+                ensemble_set_name="EnsembleSet",
+            )
+        else:
+            raise ValueError(
+                'Incorrect arguments. Either provide "csv files" or "ensembles and response_file".'
+            )
+        self.check_runs()
+        self.check_response_filters()
+        if response_ignore:
+            self.responsedf.drop(response_ignore, errors="ignore", axis=1, inplace=True)
+        if response_include:
+            self.responsedf.drop(
+                self.responsedf.columns.difference(
+                    [
+                        "REAL",
+                        "ENSEMBLE",
+                        *response_include,
+                        *list(response_filters.keys()),
+                    ]
+                ),
+                errors="ignore",
+                axis=1,
+                inplace=True,
+            )
+
+        self.plotly_theme = app.webviz_settings["theme"].plotly_theme
+        self.uid = uuid4()
+        self.set_callbacks(app)
+
+    def ids(self, element):
+        """Generate unique id for dom element"""
+        return f"{element}-id-{self.uid}"
+
+    @property
+    def tour_steps(self):
+        steps = [
+            {"id": self.ids("ensemble"), "content": ("Select the active ensemble."),},
+            {"id": self.ids("responses"), "content": ("Select the active response."),}
+        ]
+        return steps
+
+    @property
+    def responses(self):
+        """Returns valid responses. Filters out non numerical columns,
+        and filterable columns. Replaces : and , with _ to make it work with the model"""
+        responses = list(
+            self.responsedf.drop(["ENSEMBLE", "REAL"], axis=1)
+            .apply(pd.to_numeric, errors="coerce")
+            .dropna(how="all", axis="columns")
+            .columns
+        )
+        return [p for p in responses if p not in self.response_filters.keys()]
+
+    @property
+    def parameters(self):
+        """Returns numerical input parameters"""
+        parameters = list(
+            self.parameterdf.drop(["ENSEMBLE", "REAL"], axis=1)
+            .apply(pd.to_numeric, errors="coerce")
+            .dropna(how="all", axis="columns")
+            .columns
+        )
+        return parameters
+
+    @property
+    def ensembles(self):
+        """Returns list of ensembles"""
+        return list(self.parameterdf["ENSEMBLE"].unique())
+
+    def check_runs(self):
+        """Check that input parameters and response files have
+        the same number of runs"""
+        for col in ["ENSEMBLE", "REAL"]:
+            if sorted(list(self.parameterdf[col].unique())) != sorted(
+                list(self.responsedf[col].unique())
+            ):
+                raise ValueError("Parameter and response files have different runs")
+
+    def check_response_filters(self):
+        """'Check that provided response filters are valid"""
+        if self.response_filters:
+            for col_name, col_type in self.response_filters.items():
+                if col_name not in self.responsedf.columns:
+                    raise ValueError(f"{col_name} is not in response file")
+                if col_type not in ["single", "multi", "range"]:
+                    raise ValueError(
+                        f"Filter type {col_type} for {col_name} is not valid."
+                    )
+
+    @property
+    def filter_layout(self):
+        """Layout to display selectors for response filters"""
+        children = []
+        for col_name, col_type in self.response_filters.items():
+            domid = self.ids(f"filter-{col_name}")
+            values = list(self.responsedf[col_name].unique())
+            if col_type == "multi":
+                selector = wcc.Select(
+                    id=domid,
+                    options=[{"label": val, "value": val} for val in values],
+                    value=values,
+                    multi=True,
+                    size=min(20, len(values)),
+                )
+            elif col_type == "single":
+                selector = dcc.Dropdown(
+                    id=domid,
+                    options=[{"label": val, "value": val} for val in values],
+                    value=values[0],
+                    multi=False,
+                    clearable=False,
+                )
+            else:
+                return children
+            children.append(html.Div(children=[html.Label(col_name), selector,]))
+
+        return children
+
+    @property
+    def control_layout(self):
+        """Layout to select e.g. iteration and response"""
+        return [
+            html.Div(
+                [
+                    html.Label("Ensemble"),
+                    dcc.Dropdown(
+                        id=self.ids("ensemble"),
+                        options=[
+                            {"label": ens, "value": ens} for ens in self.ensembles
+                        ],
+                        clearable=False,
+                        value=self.ensembles[0],
+                    ),
+                ]
+            ),
+            html.Div(
+                [
+                    html.Label("Response"),
+                    dcc.Dropdown(
+                        id=self.ids("responses"),
+                        options=[
+                            {"label": ens, "value": ens} for ens in self.responses
+                        ],
+                        clearable=False,
+                        value=self.responses[0],
+                    ),
+                ]
+            ),
+
+        ]
+
+    @property
+    def layout(self):
+        """Main layout"""
+        return wcc.FlexBox(
+            id=self.ids("layout"),
+            children=[
+                html.Div(
+                    style={"flex": 4},
+                    children=[
+                        html.Div(
+                            id=self.ids("table_title"),
+                            style={"textAlign": "center"}
+                        ),
+                        DataTable(
+                            id=self.ids("table"),
+                            sort_action="native",
+                            filter_action="native",
+                            page_action="native",
+                            page_size=10,
+                            style_cell={"fontSize":12}
+                        )
+                    ],
+                ),
+                html.Div(
+                    style={"flex": 1},
+                    children=self.control_layout + self.filter_layout
+                    if self.response_filters
+                    else [],
+                ),
+            ],
+        )
+    @property
+    def table_input_callbacks(self):
+        """List of Inputs for multiple regression table callback"""
+        callbacks = [
+            Input(self.ids("ensemble"), "value"),
+            Input(self.ids("responses"), "value")
+        ]
+        if self.response_filters:
+            for col_name in self.response_filters:
+                callbacks.append(Input(self.ids(f"filter-{col_name}"), "value"))
+        return callbacks 
+    
+    def make_response_filters(self, filters):
+        """Returns a list of active response filters"""
+        filteroptions = []
+        if filters:
+            for i, (col_name, col_type) in enumerate(self.response_filters.items()):
+                filteroptions.append(
+                    {"name": col_name, "type": col_type, "values": filters[i]}
+                )
+        return filteroptions
+
+    def set_callbacks(self, app):
+        """Set callbacks for the table and the p-values plot"""
+        @app.callback(
+            [
+                Output(self.ids("table"), "data"),
+                Output(self.ids("table"), "columns"),
+                Output(self.ids("table_title"), "children"),
+            ],
+            self.table_input_callbacks,
+        )
+        def _update_table(ensemble, response, *filters):
+            filteroptions = self.make_response_filters(filters)
+            responsedf = filter_and_sum_responses(
+                self.responsedf,
+                ensemble,
+                response,
+                filteroptions=filteroptions,
+                aggregation=self.aggregation,
+            )
+            parameterdf = self.parameterdf.loc[self.parameterdf["ENSEMBLE"] == ensemble]
+            df = pd.merge(responsedf, parameterdf, on=["REAL"]).drop(columns=["REAL", "ENSEMBLE"])
+            
+            X = make_interaction_df(parameterdf)
+            y = df[response]
+
+            table = correlation_interactions(X, y)
+            table.index.name = "Parameter interaction terms"
+            table.reset_index(inplace=True)
+            
+            columns = [{"name": i, "id": i, 'type': 'numeric', "format": Format(precision=4)} for i in table.columns]
+            data = table.to_dict("rows")
+
+            return(
+                    data,
+                    columns,
+                    f"Correlation between {response} and parameter interaction terms"
+                )
+            
+    def add_webvizstore(self):
+        if self.parameter_csv and self.response_csv:
+            return [
+                (read_csv, [{"csv_file": self.parameter_csv,}],),
+                (read_csv, [{"csv_file": self.response_csv,}],),
+            ]
+        return [
+            (
+                load_parameters,
+                [
+                    {
+                        "ensemble_paths": self.ens_paths,
+                        "ensemble_set_name": "EnsembleSet",
+                    }
+                ],
+            ),
+            (
+                load_csv,
+                [
+                    {
+                        "ensemble_paths": self.ens_paths,
+                        "csv_file": self.response_file,
+                        "ensemble_set_name": "EnsembleSet",
+                    }
+                ],
+            ),
+        ]
+
+@CACHE.memoize(timeout=CACHE.TIMEOUT)
+def filter_and_sum_responses(
+    dframe, ensemble, response, filteroptions=None, aggregation="sum"
+):
+    """Cached wrapper for _filter_and_sum_responses"""
+    return _filter_and_sum_responses(
+        dframe=dframe,
+        ensemble=ensemble,
+        response=response,
+        filteroptions=filteroptions,
+        aggregation=aggregation,
+    )
+
+def _filter_and_sum_responses(
+    dframe, ensemble, response, filteroptions=None, aggregation="sum",
+):
+    """Filter response dataframe for the given ensemble
+    and optional filter columns. Returns dataframe grouped and
+    aggregated per realization."""
+
+    df = dframe.copy()
+    df = df.loc[df["ENSEMBLE"] == ensemble]
+    if filteroptions:
+        for opt in filteroptions:
+            if opt["type"] == "multi" or opt["type"] == "single":
+                if isinstance(opt["values"], list):
+                    df = df.loc[df[opt["name"]].isin(opt["values"])]
+                else:
+                    if opt["name"] == "DATE" and isinstance(opt["values"], str):
+                        df = df.loc[df["DATE"].astype(str) == opt["values"]]
+                    else:
+                        df = df.loc[df[opt["name"]] == opt["values"]]
+
+            elif opt["type"] == "range":
+                df = df.loc[
+                    (df[opt["name"]] >= np.min(opt["values"]))
+                    & (df[opt["name"]] <= np.max(opt["values"]))
+                ]
+    if aggregation == "sum":
+        return df.groupby("REAL").sum().reset_index()[["REAL", response]]
+    if aggregation == "mean":
+        return df.groupby("REAL").mean().reset_index()[["REAL", response]]
+    raise ValueError(
+        f"Aggregation of response file specified as '{aggregation}'' is invalid. "
+    )
+
+def make_interaction_df(parameterdf):
+    cols = [
+    'LOG10_MULTFLT:MULTFLT_F1',
+    'LOG10_MULTFLT:MULTFLT_F2',
+    'LOG10_MULTFLT:MULTFLT_F3',
+    'LOG10_MULTFLT:MULTFLT_F4',
+    'LOG10_MULTFLT:MULTFLT_F5',
+    'LOG10_MULTZ:MULTZ_MIDREEK',
+    'INTERPOLATE_GO',
+    'INTERPOLATE_WO',
+    'FWL'
+    ]
+    X = parameterdf[cols]
+
+    # The number in the dict is the depth / number of products
+    combos = {1: cols}
+    depth=3 
+
+    #Get combinations
+    for n in range(2,depth+1):   
+        combos[n]=list(itertools.combinations(cols,n))
+
+    #Create a DataFrame of parameters including the new products
+    Xc = X.copy() # Taking a copy to avoid errors of setting values at a slice of a dataframe
+    for i in range(2,depth+1):
+        for tupl in combos[i]:
+            temp = Xc[tupl[0]]
+            for j in range(1,i):
+                temp = temp*Xc[tupl[j]]
+            Xc[" * ".join(tupl)] = temp
+    return Xc
+
+def correlation_interactions(X, y):
+    df = pd.DataFrame(X.corrwith(y, method=lambda x, y: pearsonr(x,y)))
+    merged_df = pd.DataFrame()
+    for col in df.columns:
+        feature_columns = [
+            "Correlation coefficient".format(col=col), 
+            "P-value".format(col=col)
+        ]
+        merged_df[feature_columns] = df[col].apply(lambda s: pd.Series({ feature_columns[0]: s[0],
+                                                                    feature_columns[1]: s[1]} ) )
+
+    sort_by_pval_df = merged_df.sort_values(by=["P-value"]).head(20)
+
+    return sort_by_pval_df
+
+def make_range_slider(domid, values, col_name):
+    try:
+        values.apply(pd.to_numeric, errors="raise")
+    except ValueError:
+        raise ValueError(
+            f"Cannot calculate filter range for {col_name}. "
+            "Ensure that it is a numerical column."
+        )
+    return dcc.RangeSlider(
+        id=domid,
+        min=values.min(),
+        max=values.max(),
+        step=calculate_slider_step(
+            min_value=values.min(),
+            max_value=values.max(),
+            steps=len(list(values.unique())) - 1,
+        ),
+        value=[values.min(), values.max()],
+        marks={
+            str(values.min()): {"label": f"{values.min():.2f}"},
+            str(values.max()): {"label": f"{values.max():.2f}"},
+        },
+    )
+
+def theme_layout(theme, specific_layout):
+    layout = {}
+    layout.update(theme["layout"])
+    layout.update(specific_layout)
+    return layout
+
+
+@CACHE.memoize(timeout=CACHE.TIMEOUT)
+@webvizstore
+def read_csv(csv_file) -> pd.DataFrame:
+    return pd.read_csv(csv_file, index_col=False)
