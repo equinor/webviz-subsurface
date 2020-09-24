97dc367e4b9b8d5d4907bbee4cdf1a8a03ae3984
Asgeir Nyvoll <47146384+asnyv@users.noreply.github.com>
2020-09-24 14:51:01 +0200

    Add free space and new design to DiskUsage (#439)

--- webviz_subsurface/plugins/_disk_usage.py
+++ webviz_subsurface/plugins/_disk_usage.py
@@ -1,20 +1,19 @@
+import shutil
+import warnings
 import pathlib
 import datetime
 from typing import Optional
 
 import pandas as pd
 import dash_html_components as html
-import dash_core_components as dcc
 import webviz_core_components as wcc
-from dash.dependencies import Input, Output
 from webviz_config.webviz_store import webvizstore
 from webviz_config.common_cache import CACHE
 from webviz_config import WebvizPluginABC
 
 
 class DiskUsage(WebvizPluginABC):
-    """Visualize disk usage in a FMU project. It adds a dashboard showing disk usage per user,
-        where the user can choose to plot as a pie chart or as a bar chart.
+    """Visualize disk usage in a FMU project. It adds a dashboard showing disk usage per user.
 
     ---
 
@@ -29,8 +28,9 @@ class DiskUsage(WebvizPluginABC):
     The plugin will search backwards from the current date, and throw an error if no file was found
     from the last week.
 
-    The csv file must have the columns `userid` and `usageKB` (where KB means kilobytes).
-    All other columns are ignored."""
+    The csv file must have the columns `userid` and `usageKB` (where KB means
+    [kibibytes](https://en.wikipedia.org/wiki/Kibibyte)). All other columns are ignored.
+    """
 
     def __init__(self, app, scratch_dir: pathlib.Path, date: Optional["str"] = None):
 
@@ -41,71 +41,64 @@ class DiskUsage(WebvizPluginABC):
         self.disk_usage = get_disk_usage(self.scratch_dir, self.date_input)
         self.date = str(self.disk_usage["date"].unique()[0])
         self.users = self.disk_usage["userid"]
-        self.usage_gb = self.disk_usage["usageKB"] / (1024 ** 2)
-        self.set_callbacks(app)
+        self.usage_gib = self.disk_usage["usageKB"] / (1024 ** 2)
         self.theme = app.webviz_settings["theme"]
 
     @property
     def layout(self):
         return html.Div(
             [
-                html.P(
-                    f"This is the disk usage on \
-                        {self.scratch_dir} per user, \
-                        as of {self.date}."
+                html.H5(
+                    f"Disk usage on {self.scratch_dir} per user as of {self.date}",
+                    style={"text-align": "center"},
                 ),
-                dcc.RadioItems(
-                    id=self.uuid("plot_type"),
-                    options=[
-                        {"label": i, "value": i} for i in ["Pie chart", "Bar chart"]
-                    ],
-                    value="Pie chart",
-                    persistence=True,
-                    persistence_type="session",
+                wcc.FlexBox(
+                    children=[
+                        wcc.Graph(
+                            style={"flex": 1},
+                            figure=self.pie_chart,
+                            config={"displayModeBar": False},
+                        ),
+                        wcc.Graph(style={"flex": 2}, figure=self.bar_chart),
+                    ]
                 ),
-                wcc.Graph(id=self.uuid("chart")),
             ]
         )
 
-    def set_callbacks(self, app):
-        @app.callback(
-            Output(self.uuid("chart"), "figure"),
-            [Input(self.uuid("plot_type"), "value")],
-        )
-        def _update_plot(plot_type):
-            if plot_type == "Pie chart":
-                data = [
-                    {
-                        "values": self.usage_gb,
-                        "labels": self.users,
-                        "text": (self.usage_gb).map("{:.2f} GB".format),
-                        "textinfo": "label",
-                        "textposition": "inside",
-                        "hoverinfo": "label+text",
-                        "type": "pie",
-                    }
-                ]
-                layout = {}
-
-            elif plot_type == "Bar chart":
-                data = [
-                    {
-                        "y": self.usage_gb,
-                        "x": self.users,
-                        "text": (self.usage_gb).map("{:.2f} GB".format),
-                        "hoverinfo": "x+text",
-                        "type": "bar",
-                    }
-                ]
-                layout = {
-                    "yaxis": {"title": "Usage in Gigabytes"},
-                    "xaxis": {"title": "User name"},
+    @property
+    def pie_chart(self):
+        return {
+            "data": [
+                {
+                    "values": self.usage_gib,
+                    "labels": self.users,
+                    "pull": (self.users.values == "Free space") * 0.05,
+                    "text": (self.usage_gib).map("{:.2f} GiB".format),
+                    "textinfo": "label",
+                    "textposition": "inside",
+                    "hoverinfo": "label+text",
+                    "type": "pie",
                 }
+            ],
+            "layout": self.theme.create_themed_layout({}),
+        }
 
-            layout["height"] = 800
-            layout["width"] = 1000
-
-            return {"data": data, "layout": self.theme.create_themed_layout(layout)}
+    @property
+    def bar_chart(self):
+        return {
+            "data": [
+                {
+                    "y": self.usage_gib,
+                    "x": self.users,
+                    "text": (self.usage_gib).map("{:.2f} GiB".format),
+                    "hoverinfo": "x+text",
+                    "type": "bar",
+                }
+            ],
+            "layout": self.theme.create_themed_layout(
+                {"yaxis": {"title": "GiB (GibiBytes)"}}
+            ),
+        }
 
     def add_webvizstore(self):
         return [
@@ -119,7 +112,7 @@ class DiskUsage(WebvizPluginABC):
 @CACHE.memoize(timeout=CACHE.TIMEOUT)
 @webvizstore
 def get_disk_usage(scratch_dir, date) -> pd.DataFrame:
-    if date is None:
+    def _loop_dates(scratch_dir):
         today = datetime.datetime.today()
         for i in range(7):
             date = today - datetime.timedelta(days=i)
@@ -134,11 +127,41 @@ def get_disk_usage(scratch_dir, date) -> pd.DataFrame:
         raise FileNotFoundError(
             f"No disk usage file found for last week in {scratch_dir}."
         )
-    try:
-        return pd.read_csv(
-            scratch_dir / ".disk_usage" / f"disk_usage_user_{date}.csv"
-        ).assign(date=date)
-    except FileNotFoundError as exc:
-        raise FileNotFoundError(
-            f"No disk usage file found for {date} in {scratch_dir}."
-        ) from exc
+
+    if date is None:
+        df = _loop_dates(scratch_dir)
+    else:
+        try:
+            df = pd.read_csv(
+                scratch_dir / ".disk_usage" / f"disk_usage_user_{date}.csv"
+            ).assign(date=date)
+        except FileNotFoundError as exc:
+            raise FileNotFoundError(
+                f"No disk usage file found for {date} in {scratch_dir}."
+            ) from exc
+
+    free_space_kib = (shutil.disk_usage(scratch_dir).total / 1024) - df["usageKB"].sum()
+
+    if free_space_kib < 0:
+        warnings.warn(
+            f"Reported disk usage in "
+            f"{scratch_dir}/.disk_usage/disk_usage_user_{df['date'].unique()[0]}.csv "
+            f"must be wrong (unless total disk size has changed after {df['date'].unique()[0]}). "
+            f"Total reported usage is {int(df['usageKB'].sum() / 1024**2)} GiB, "
+            f"but the disk size is only {int(shutil.disk_usage(scratch_dir).total / 1024**3)} GiB "
+            f"(this would imply negative free space of {int(free_space_kib / 1024**2)} GiB).",
+            UserWarning,
+            stacklevel=0,
+        )
+    else:
+        df = df.append(
+            {
+                "userid": "<b>Free space</b>",
+                "usageKB": shutil.disk_usage(scratch_dir).total / 1024
+                - df["usageKB"].sum(),
+                "date": df["date"].unique()[0],
+            },
+            ignore_index=True,
+        )
+
+    return df.sort_values(by="usageKB", axis=0, ascending=False)
