0bc3eb31cc2884256d894753d9f5928471793e43
Anders Fredrik Kiær <akia@equinor.com>
2020-10-29 11:20:20 +0100

    fix

--- tests/integration_tests/test_aggregated_data_example.py
+++ tests/integration_tests/test_aggregated_data_example.py
@@ -1,6 +1,7 @@
 import sys
 import subprocess  # nosec
 
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.common.exceptions import (
