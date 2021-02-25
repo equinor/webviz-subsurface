a3bdba396bccd2afcacb2d4e56e44425d6bc55b6
Hans Kallekleiv <hakal@equinor.com>
2021-02-25 12:17:02 +0100

    wip

--- webviz_subsurface/_models/table_model.py
+++ webviz_subsurface/_models/table_model.py
@@ -2,50 +2,76 @@ from typing import List
 import pandas as pd
 
 
-class TableModel:
-    def __init__(
-        self,
-        dataframe: pd.DataFrame,
-        selector_columns: List = None,
-        filter_columns: List = None,
-    ) -> None:
-        self._dataframe = dataframe
-        self._selector_columns = selector_columns if selector_columns else []
-        self._filter_columns = filter_columns if filter_columns else []
-        self._validate_input()
+class EnsembleTableModel
+    def column_names() -> List[str]: ...
+    def has_column(str) -> boolean: ...
+    def has_columns(List[str]) -> boolean: ...
 
-    @property
+    def realizations() -> List[int]: ...
+
+    def get_column_values(column_name, realizations: Optional[Sequence[int]]) ->
+    def get_realizations_based_on_filter(filter_column_name: str, column_values: list) -> Sequence[int]: ...
+
+
+
+
+class EnsembleSetTableModel
+    @staticmethod
+    def fromEnsembleLayout(ensembles: Dict[str, path], csv_file, selector_columns, filter_columns) -> EnsembleSetTableModel: 
+        ensembleset = EnsembleSetTableModel()
+        for ens in ensemble
+            new_ensemble = EnsembleModel_ensembleLayout()
+            ensembleset.add_ensemble(new_ensemble)
+        return ensembleset
+
+    @staticmethod
+    def fromAggrCsvFile(aggr_csv_file, selector_columns, filter_columns) -> EnsembleSetTableModel: ...
+        # Read CSV file
+        # Split into ensembles
+        # Create EnsembleModel_csvBased for each ensemble
+        
+        ensembleset = EnsembleSetTableModel()
+        return ensembleset
+
+    def ensemble_names(self) -> List[str]: ...
+    def ensemble(name) -> EnsembleTable: ...
+
+    # Tja...
     def selector_columns(self) -> List:
-        return self._selector_columns
-
-    @property
     def filter_columns(self) -> List:
-        return self._filter_columns
 
-    @property
-    def ensembles(self) -> List:
-        return list(self._dataframe["ENSEMBLE"].unique())
 
-    def realizations_in_ensemble(self, ensemble: str) -> List:
-        return list(
-            self._dataframe[self._dataframe["ENSEMBLE"] == ensemble]["REAL"].unique()
-        )
 
-    def _validate_input(self):
-        for column in ["ENSEMBLE", "REAL"]:
-            if column not in self._dataframe.columns:
-                raise KeyError(f"Required column {column} is missing from input data")
-        for column in self.selector_columns:
-            if column not in self._dataframe.columns:
-                raise KeyError(
-                    f"Specified selector {column} is missing from input data"
-                )
-        for column in self.filter_columns:
-            if column not in self._dataframe.columns:
-                raise KeyError(f"Specified filter {column} is missing from input data")
+class EnsembleModel_ensembleLayout(EnsembleTableModel):
+class EnsembleModel_csvBased(EnsembleTableModel):
 
-    def get_column_values_for_ensemble(
-        self, ensemble: str, column: str, realizations: List = None
-    ):
-        if column not in self._dataframe.columns:
-            raise KeyError(f"column {column} is missing from input data")
+
+
+
+
+
+class Plugin:
+    def __init__(self, ensembles, csv_file, aggr_csv_file):
+
+        if (ensembles and fromEnsembleLayout):
+            self.enssettablemodel = EnsembleSetTableModel.fromEnsembleLayout(ensembles, csv_file)
+        else:
+            self.enssettablemodel = EnsembleSetTableModel.fromAggrCsvFile(aggr_csv_file)
+
+    # Long term :-)
+    #def __init__(self, ensemble_set_table_model: EnsembleSetTableModele):
+    #    self.enssettablemodel = ensemble_set_table_model
+
+
+
+    def layout:
+
+        dropdown("available ensembles")
+        dropdown("columns available for plotting")
+        dropdown("filter on value in some column")
+        figure
+
+    def callbacks:
+        
+
+        
