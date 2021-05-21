62f9877b67c9cb7f9fd258d1871d073d81b8d3f3
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2021-05-21 11:42:16 +0200

    Added detailed timings in __init__

--- webviz_subsurface/_providers/ensemble_table_provider_impl_arrow.py
+++ webviz_subsurface/_providers/ensemble_table_provider_impl_arrow.py
@@ -25,7 +25,10 @@ class EnsembleTableProviderImplArrow(EnsembleTableProvider):
 
         # We'll try and keep the file open for the life-span of the provider
         source = pa.memory_map(self._arrow_file_name, "r")
+        et_open_ms = timer.lap_ms()
+
         self._cached_reader = pa.ipc.RecordBatchFileReader(source)
+        et_create_reader_ms = timer.lap_ms()
 
         # Discover columns and realizations that are present in the arrow file
         column_names_on_file = self._cached_reader.schema.names
@@ -34,14 +37,18 @@ class EnsembleTableProviderImplArrow(EnsembleTableProvider):
             for colname in column_names_on_file
             if colname not in ["REAL", "ENSEMBLE"]
         ]
+        et_find_col_names_ms = timer.lap_ms()
 
         unique_realizations_on_file = (
             self._cached_reader.read_all().column("REAL").unique()
         )
         self._realizations: List[int] = unique_realizations_on_file.to_pylist()
+        et_find_real_ms = timer.lap_ms()
 
         LOGGER.debug(
             f"init took: {timer.elapsed_s():.2f}s, "
+            f"(open={et_open_ms}ms, create_reader={et_create_reader_ms}ms, "
+            f"find_col_names={et_find_col_names_ms}ms, find_real={et_find_real_ms}ms), "
             f"#column_names={len(self._column_names)}, "
             f"#realization={len(self._realizations)}"
         )
@@ -108,19 +115,19 @@ class EnsembleTableProviderImplArrow(EnsembleTableProvider):
         )
 
         table = self._cached_reader.read_all().select(columns_to_get)
-        read_ms = timer.lap_ms()
+        et_read_ms = timer.lap_ms()
 
         if realizations:
             mask = pc.is_in(table["REAL"], value_set=pa.array(realizations))
             table = table.filter(mask)
-        filter_ms = timer.lap_ms()
+        et_filter_ms = timer.lap_ms()
 
         df = table.to_pandas(ignore_metadata=True)
-        to_pandas_ms = timer.lap_ms()
+        et_to_pandas_ms = timer.lap_ms()
 
         LOGGER.debug(
             f"get_column_data() took: {timer.elapsed_ms()}ms "
-            f"(read={read_ms}ms, filter={filter_ms}ms, to_pandas={to_pandas_ms}ms), "
+            f"(read={et_read_ms}ms, filter={et_filter_ms}ms, to_pandas={et_to_pandas_ms}ms), "
             f"#cols={len(column_names)}, "
             f"#real={len(realizations) if realizations else 'all'}, "
             f"df.shape={df.shape}, file={Path(self._arrow_file_name).name}"
