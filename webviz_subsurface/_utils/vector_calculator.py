fbf2a0e3cab23ed8871975e979ba184fc829e0db
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2021-05-31 15:59:20 +0200

    Make vector caclulations on demand and refactor helpers
    - Calculate vectors on demand (stateless)
    -Move helper functionality to vector calculator wrapper

--- webviz_subsurface/_utils/vector_calculator.py
+++ webviz_subsurface/_utils/vector_calculator.py
@@ -1,29 +1,29 @@
 from typing import List, Dict
 import pandas as pd
+
+from webviz_config.common_cache import CACHE
+
 from webviz_subsurface_components import (
     VectorCalculator,
     ExpressionInfo,
-    VariableVectorMapInfo,
 )
 
 
-def get_expression_with_vector_names(expression: ExpressionInfo) -> str:
-    res: str = expression["expression"]
-    var_vec_map: List[VariableVectorMapInfo] = expression["variableVectorMap"]
-    for var_vec_pair in var_vec_map:
-        res = res.replace(var_vec_pair["variableName"], var_vec_pair["vectorName"][0])
-    return res
+@CACHE.memoize(timeout=CACHE.TIMEOUT)
+def get_selected_expressions(
+    expressions: List[ExpressionInfo], selected_names: List[str]
+) -> List[ExpressionInfo]:
+    selected: List[ExpressionInfo] = []
+    for name in selected_names:
+        selected_expression: ExpressionInfo = next(
+            (elm for elm in expressions if elm["name"] == name), None
+        )
+        if selected_expression is not None and selected_expression not in selected:
+            selected.append(selected_expression)
+    return selected
 
 
-def get_parser_values(
-    var_vec_dict: Dict[str, str], smry: pd.DataFrame
-) -> Dict[str, pd.Series]:
-    res: Dict[str, pd.Series] = {}
-    for var_name in var_vec_dict:
-        res[var_name] = smry[var_vec_dict[var_name]]
-    return res
-
-
+@CACHE.memoize(timeout=CACHE.TIMEOUT)
 def get_calculated_vectors(
     expressions: List[ExpressionInfo],
     smry: pd.DataFrame,
@@ -34,11 +34,14 @@ def get_calculated_vectors(
     for elm in expressions:
         name: str = elm["name"]
         expr: str = elm["expression"]
-        var_vec_map = elm["variableVectorMap"]
-
-        values: Dict[str, pd.Series] = get_parser_values(
-            VectorCalculator.get_var_vec_dict(var_vec_map), smry
+        var_vec_dict: Dict[str, str] = VectorCalculator.get_var_vec_dict(
+            elm["variableVectorMap"]
         )
+
+        values: Dict[str, pd.Series] = {}
+        for var in var_vec_dict:
+            values[var] = smry[var_vec_dict[var]]
+
         parsed_expr = VectorCalculator.parse_expression(expr, values)
         if parsed_expr is not None:
             calculated_vectors[name] = parsed_expr
