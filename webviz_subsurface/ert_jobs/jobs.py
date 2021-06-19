65c3cac887fc7824d58404293b322f5e486b364a
Øyvind Lind-Johansen (CCI RPT RES1) <olind@st-linrgsn162.st.statoil.no>
2021-06-19 10:50:01 +0200

    trying to fix decorator issue

--- webviz_subsurface/ert_jobs/jobs.py
+++ webviz_subsurface/ert_jobs/jobs.py
@@ -26,10 +26,8 @@ except ModuleNotFoundError:
         return decorator
 
 
-@hook_implementation
-@plugin_response(
-    plugin_name="webviz-subsurface"
-)  # pylint: disable=no-value-for-parameter
+@hook_implementation  # pylint: disable=no-value-for-parameter
+@plugin_response(plugin_name="webviz-subsurface")
 def installable_jobs() -> dict:
     resource_directory = Path(
         resource_filename("webviz_subsurface", "ert_jobs/config_jobs")
