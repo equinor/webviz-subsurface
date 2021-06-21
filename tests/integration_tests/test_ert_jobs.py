46f249a78b9e9b5324ffbdb5e3f95f0601787581
Øyvind Lind-Johansen (CCI RPT RES1) <olind@st-linrgsn162.st.statoil.no>
2021-06-21 11:23:13 +0200

    added assert statements to test

--- tests/integration_tests/test_ert_jobs.py
+++ tests/integration_tests/test_ert_jobs.py
@@ -1,19 +1,29 @@
 import subprocess  # nosec
 from pathlib import Path
 
+import pandas as pd
+
 
 def test_export_connection_status(testdata_folder: Path, tmp_path: Path) -> None:
 
+    runpath = f"{testdata_folder}/reek_history_match/realization-0/iter-0/"
     ert_config = f"""
 ECLBASE     something\n
-RUNPATH     {testdata_folder}/reek_history_match/realization-0/iter-0/\n
+RUNPATH     {runpath}\n
 NUM_REALIZATIONS    1\n
 QUEUE_OPTION   LSF MAX_RUNNING 1\n
 QUEUE_SYSTEM LOCAL\n
 FORWARD_MODEL EXPORT_CONNECTION_STATUS(<INPUT>=share/results/tables/summary.parquet, <OUTPUT>=output.parquet)\n
 """
+
     ert_config_file = tmp_path / "config.ert"
     with open(ert_config_file, "w") as file:
         file.write(ert_config)
 
     subprocess.call(["ert", "test_run", ert_config_file])  # nosec
+
+    output_file = Path(f"{runpath}output.parquet")
+    assert output_file.exists()
+
+    df = pd.read_parquet(output_file)
+    assert list(df.columns) == ["DATE", "WELL", "I", "J", "K", "OP/SH"]
