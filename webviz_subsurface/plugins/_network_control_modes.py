590ff98e649eebc62befec80192a9b07ae4fe880
Øyvind Lind-Johansen (CCI RPT RES1) <olind@st-linrgsn189.st.statoil.no>
2021-07-15 14:03:34 +0200

    various updates

--- webviz_subsurface/plugins/_network_control_modes.py
+++ webviz_subsurface/plugins/_network_control_modes.py
@@ -2,6 +2,8 @@ from typing import List, Dict, Union, Tuple, Callable
 from pathlib import Path
 
 import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.express as px
 import numpy as np
 import pandas as pd
 import dash
@@ -19,10 +21,9 @@ import webviz_subsurface
 from webviz_subsurface._models import EnsembleSetModel
 from webviz_subsurface._models import caching_ensemble_set_model_factory
 
-class NetworkControlModes(WebvizPluginABC):
-    """Description
 
-    """
+class NetworkControlModes(WebvizPluginABC):
+    """Description"""
 
     def __init__(
         self,
@@ -45,13 +46,21 @@ class NetworkControlModes(WebvizPluginABC):
                     for ens in ensembles
                 },
                 time_index=self.time_index,
-                column_keys=["WMCTL:*", "GMCT*", "FMCT*", "GPR:*", ]
+                column_keys=[
+                    "WMCTL:*",
+                    "GMCT*",
+                    "FMCT*",
+                    "GPR:*",
+                ],
             )
         )
         self.smry = self.emodel.get_or_load_smry_cached()
         self.ensembles = list(self.smry["ENSEMBLE"].unique())
         WEBVIZ_ASSETS.add(
-            Path(webviz_subsurface.__file__).parent / "_assets" / "css" / "container.css"
+            Path(webviz_subsurface.__file__).parent
+            / "_assets"
+            / "css"
+            / "container.css"
         )
         self.set_callbacks(app)
 
@@ -69,70 +78,69 @@ class NetworkControlModes(WebvizPluginABC):
         return wcc.FlexBox(
             id=self.uuid("layout"),
             children=[
-                html.Div(
-                    style={"flex": 1},
-                    children=[
-                        self.selectors_layout()
-                    ]
+                wcc.FlexColumn(
+                    flex=1, style={"height": "90vh"}, children=[self.selectors_layout()]
                 ),
-                html.Div(
-                    className="framed",
-                    style={"flex": 3, "height": "89vh"},
+                wcc.FlexColumn(
+                    flex=4,
                     children=[
-                        html.Div(
-                            children=wcc.Graph(id=self.uuid("ctrlmode_areachart"))
+                        wcc.Frame(
+                            style={"height": "90vh"},
+                            highlight=False,
+                            color="white",
+                            children=wcc.Graph(
+                                style={"height": "85vh"}, id=self.uuid("graph")
+                            ),
                         )
-                    ]
-                )
-            ]
+                    ],
+                ),
+            ],
         )
-
-
 
     def selectors_layout(self):
         return html.Div(
-            className = "framed",
+            className="framed",
             style={"fontSize": "14"},
             children=[
-                dropdown_for_plotly_data(
-                    uuid=self.uuid("ensemble_dropdown"),
-                    title="Ensemble",
-                    options=[{"label": col, "value": col} for col in self.ensembles],
-                    value=self.ensembles[0],
-                    multi=False,
+                wcc.Selectors(
+                    label="Ensemble",
+                    children=wcc.Dropdown(
+                        id=self.uuid("ensemble_dropdown"),
+                        options=[
+                            {"label": col, "value": col} for col in self.ensembles
+                        ],
+                        value=self.ensembles[0],
+                        multi=False,
+                    ),
                 ),
-                html.Div(
-                    id=self.uuid("node_type"),
-                    style={"marginTop": "15px"},
-                    children=[
-                        wcc.RadioItems(
-                            label = "Node type",
-                            vertical = False,
-                            wrapper_id = self.uuid("node_type_radioitems"),
-                            options = [
-                                {
-                                    "label": "Well",
-                                    "value": "well",
-                                },
-                                {
-                                    "label": "Field/Group",
-                                    "value": "field_group",
-                                }
-                            ],
-                            value="well",
-                        )
-                    ]
+                wcc.Selectors(
+                    label="Node type:",
+                    children=wcc.RadioItems(
+                        id=self.uuid("node_type_radioitems"),
+                        options=[
+                            {
+                                "label": "Well",
+                                "value": "well",
+                            },
+                            {
+                                "label": "Field/group",
+                                "value": "field_group",
+                            },
+                        ],
+                        value="well",
+                    ),
                 ),
-                dropdown_for_plotly_data(
-                    uuid=self.uuid("node_dropdown"),
-                    title="Node",
-                    options=[],
-                    value=None,
-                    multi=False,
+                wcc.Selectors(
+                    label="Node",
+                    children=wcc.Dropdown(
+                        id=self.uuid("node_dropdown"),
+                        options=[],
+                        value=None,
+                        multi=False,
+                    ),
                 ),
-            ]
+            ],
         )
-
 
     def set_callbacks(self, app: dash.Dash) -> None:
         @app.callback(
@@ -143,13 +151,17 @@ class NetworkControlModes(WebvizPluginABC):
         )
         def _update_node_dropdown(ensemble: str, node_type: str) -> list:
             print("update node dropdown")
-            smry = self.smry[self.smry.ENSEMBLE==ensemble].copy()
-            smry.dropna(how='all', axis=1, inplace=True)
+            smry = self.smry[self.smry.ENSEMBLE == ensemble].copy()
+            smry.dropna(how="all", axis=1, inplace=True)
 
             if node_type == "well":
-                nodes = [vec.split(":")[1] for vec in smry.columns if vec.startswith("WMCTL")]
+                nodes = [
+                    vec.split(":")[1] for vec in smry.columns if vec.startswith("WMCTL")
+                ]
             elif node_type == "field_group":
-                nodes = [vec.split(":")[1] for vec in smry.columns if vec.startswith("GMCTP")]
+                nodes = [
+                    vec.split(":")[1] for vec in smry.columns if vec.startswith("GMCTP")
+                ] + ["FIELD"]
             else:
                 raise ValueError(f"Node type {node_type} not implemented.")
             if not nodes:
@@ -157,43 +169,96 @@ class NetworkControlModes(WebvizPluginABC):
             return [{"label": node, "value": node} for node in nodes], nodes[0]
 
         @app.callback(
-            Output(self.uuid("ctrlmode_areachart"), "figure"),
+            Output(self.uuid("graph"), "figure"),
             Input(self.uuid("ensemble_dropdown"), "value"),
             Input(self.uuid("node_type_radioitems"), "value"),
             Input(self.uuid("node_dropdown"), "value"),
         )
-        def _make_area_chart(ensemble: str, node_type: str, node: str):
+        def _update_graph(ensemble: str, node_type: str, node: str):
             print("make chart")
-            sumvec = f"WMCTL:{node}"
-            if not sumvec in self.smry.columns:
+
+            if ensemble is None or node_type is None or node is None:
+                # Format this a bit more
                 return go.Figure()
-            smry = self.smry[self.smry.ENSEMBLE==ensemble][["DATE", sumvec]]
-            df = smry.groupby("DATE")[sumvec].value_counts().unstack().fillna(0).reset_index()
-            df["Other"] = 0
-            categories = get_ctrlmode_categories(node_type)
 
-            fig = go.Figure()
-
-            for col in [col for col in df.columns if not col in ["DATE", "Other"]]:
-                if str(col) in categories:
-                    name = categories[str(col)]["name"]
-                    color = categories[str(col)]["color"]
-                    add_trace(fig, df.DATE, df[col], name, color)
-                else:
-                    df.Other = df.Other + df[col]
-            #df.to_csv("plotdata.csv")
-            if df.Other.sum()>0:
-                add_trace(fig, df.DATE, df.Other, categories["Other"]["name"], categories["Other"]["color"])
-
-            fig.update_layout(
-                title_text="Number of realizations on different control modes",
-                yaxis_title="# realizations",
-                margin=dict(t=60),
-                yaxis=dict(range=[0,self.smry.REAL.nunique()]),
-                paper_bgcolor="rgba(0,0,0,0)",
-                plot_bgcolor="rgba(0,0,0,0)",
+            fig = make_subplots(
+                rows=2,
+                shared_xaxes=False,
+                vertical_spacing=0.05,
+                subplot_titles=[
+                    "Number of realizations on different control modes",
+                    "Node pressures",
+                ],
             )
+
+            fig.add_traces(
+                [
+                    make_area_chart(ensemble, node_type, node, self.smry),
+                    make_node_pressure_graph(),
+                ],
+                rows=[1, 2],
+            )
+
             return fig
+
+
+def make_node_pressure_graph():
+    """Description"""
+    return go.Figure()
+
+
+def make_area_chart(ensemble: str, node_type: str, node: str, smry: pd.DataFrame):
+    """Description"""
+
+    ctrl_mode_sumvec = sumvec = get_sumvec_from_nodetype_and_name(node_type, node)
+    sumvec = get_sumvec_from_nodetype_and_name(node_type, node)
+    fig = go.Figure
+    if not sumvec in smry.columns:
+        print(f"{sumvec} not in emsemble")
+        return fig
+
+    smry = smry[smry.ENSEMBLE == ensemble][["DATE", sumvec, "REAL"]]
+    df = smry.groupby("DATE")[sumvec].value_counts().unstack().fillna(0).reset_index()
+    return px.area(smry, x="DATE", y=sumvec, color="REAL")
+    # df["Other"] = 0
+    # categories = get_ctrlmode_categories(node_type)
+
+    # for col in [col for col in df.columns if not col in ["DATE", "Other"]]:
+    #     if str(col) in categories:
+    #         name = categories[str(col)]["name"]
+    #         color = categories[str(col)]["color"]
+    #         #add_trace(fig, df.DATE, df[col], name, color)
+    #     else:
+    #         df.Other = df.Other + df[col]
+
+    # if df.Other.sum()>0:
+    #    add_trace(fig, df.DATE, df.Other, categories["Other"]["name"], categories["Other"]["color"])
+
+    # fig.update_layout(
+    #     title_text="Number of realizations on different control modes",
+    #     yaxis_title="# realizations",
+    #     margin=dict(t=60),
+    #     yaxis=dict(range=[0,self.smry.REAL.nunique()]),
+    #     paper_bgcolor="rgba(0,0,0,0)",
+    #     plot_bgcolor="rgba(0,0,0,0)",
+    # )
+
+    # return fig
+
+
+def get_sumvec_from_nodetype_and_name(node_type: str, node: str):
+    """Description"""
+    if node is None:
+        return None
+    elif node == "FIELD":
+        return "FMCTP"
+    elif node_type == "well":
+        return f"WMCTL:{node}"
+    elif node_type == "field_group":
+        return f"GMCTP:{node}"
+    else:
+        raise ValueError(f"Node type {node_type} not implemented")
+
 
 def add_trace(fig, x_series, y_series, name, color):
     """Description"""
@@ -209,125 +274,34 @@ def add_trace(fig, x_series, y_series, name, color):
         )
     )
 
-# pylint: disable=too-many-arguments
-def dropdown_for_plotly_data(
-    uuid: str,
-    title: str,
-    options: List[Dict],
-    value: Union[List, str] = None,
-    flex: int = 1,
-    placeholder: str = "Select...",
-    multi: bool = False,
-    clearable: bool = False,
-) -> html.Div:
-    return html.Div(
-        style={"flex": flex},
-        children=[
-            html.Label(
-                title, style={"backgroundColor": "transparent", "fontWeight": "bold"}
-            ),
-            dcc.Dropdown(
-                style={"backgroundColor": "transparent"},
-                id=uuid,
-                options=options,
-                value=value,
-                clearable=clearable,
-                placeholder=placeholder,
-                multi=multi,
-                persistence=True,
-                persistence_type="session",
-            ),
-        ],
-    )
-
 
 def get_ctrlmode_categories(node_type):
     """Description"""
     if node_type == "well":
         return {
-            "0.0":{
-                "name": "SHUT/STOP",
-                "color": "#302f2f" #grey
-            },
-            "1.0":{
-                "name": "ORAT",
-                "color": "#044a2e" #green
-            },
-            "2.0":{
-                "name": "WRAT",
-                "color": "#10026b" #blue
-            },
-            "3.0":{
-                "name": "GRAT",
-                "color": "#7a0202" #red
-            },
-            "4.0":{
-                "name": "LRAT",
-                "color": "#b06d15" #muted purple
-            },
-            "5.0":{
-                "name": "RESV",
-                "color": "#67ab99" #green/blue
-            },
-            "6.0":{
-                "name": "THP",
-                "color": "#7e5980" #purple
-            },
-            "7.0":{
-                "name": "BHP",
-                "color": "#1f77b4" #muted blue
-            },
-            "-1.0":{
-                "name": "GRUP",
-                "color": "#cfcc74" #yellow
-            },
-            "Other":{
-                "name": "Other",
-                "color": "#ffffff" #white
-            }
+            "0.0": {"name": "SHUT/STOP", "color": "#302f2f"},  # grey
+            "1.0": {"name": "ORAT", "color": "#044a2e"},  # green
+            "2.0": {"name": "WRAT", "color": "#10026b"},  # blue
+            "3.0": {"name": "GRAT", "color": "#7a0202"},  # red
+            "4.0": {"name": "LRAT", "color": "#b06d15"},  # muted purple
+            "5.0": {"name": "RESV", "color": "#67ab99"},  # green/blue
+            "6.0": {"name": "THP", "color": "#7e5980"},  # purple
+            "7.0": {"name": "BHP", "color": "#1f77b4"},  # muted blue
+            "-1.0": {"name": "GRUP", "color": "#cfcc74"},  # yellow
+            "Other": {"name": "Other", "color": "#ffffff"},  # white
         }
     elif node_type == "field_group":
         return {
-            "0.0":{
-                "name": "NONE",
-                "color": "#302f2f" #grey
-            },
-            "1.0":{
-                "name": "ORAT",
-                "color": "#044a2e" #green
-            },
-            "2.0":{
-                "name": "WRAT",
-                "color": "#10026b" #blue
-            },
-            "3.0":{
-                "name": "GRAT",
-                "color": "#7a0202" #red
-            },
-            "4.0":{
-                "name": "LRAT",
-                "color": "#b06d15" #muted purple
-            },
-            "5.0":{
-                "name": "RESV",
-                "color": "#67ab99" #green/blue
-            },
-            "6.0":{
-                "name": "PRBL",
-                "color": "#7e5980" #purple
-            },
-            "7.0":{
-                "name": "ENERGY",
-                "color": "#1f77b4" #muted blue
-            },
-            "-ve":{
-                "name": "GRUP",
-                "color": "#cfcc74" #yellow
-            },
-            "Other":{
-                "name": "Other",
-                "color": "#ffffff" #white
-            }
+            "0.0": {"name": "NONE", "color": "#302f2f"},  # grey
+            "1.0": {"name": "ORAT", "color": "#044a2e"},  # green
+            "2.0": {"name": "WRAT", "color": "#10026b"},  # blue
+            "3.0": {"name": "GRAT", "color": "#7a0202"},  # red
+            "4.0": {"name": "LRAT", "color": "#b06d15"},  # muted purple
+            "5.0": {"name": "RESV", "color": "#67ab99"},  # green/blue
+            "6.0": {"name": "PRBL", "color": "#7e5980"},  # purple
+            "7.0": {"name": "ENERGY", "color": "#1f77b4"},  # muted blue
+            "-ve": {"name": "GRUP", "color": "#cfcc74"},  # yellow
+            "Other": {"name": "Other", "color": "#ffffff"},  # white
         }
     else:
         raise ValueError(f"Node type: {node_type} not implemented")
