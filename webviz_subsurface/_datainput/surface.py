466a425567eb53a203077215aa8f8a4b37195099
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2021-08-11 15:55:05 +0200

    Initialize RegularSurface using from_file()

--- webviz_subsurface/_datainput/surface.py
+++ webviz_subsurface/_datainput/surface.py
@@ -1,13 +1,13 @@
 import numpy as np
-from xtgeo import RegularSurface
+import xtgeo
 from webviz_config.common_cache import CACHE
 
 
 @CACHE.memoize(timeout=CACHE.TIMEOUT)
-def load_surface(surface_path: str) -> RegularSurface:
-    return RegularSurface(surface_path)
+def load_surface(surface_path: str) -> xtgeo.RegularSurface:
+    return xtgeo.surface_from_file(surface_path)
 
 
 @CACHE.memoize(timeout=CACHE.TIMEOUT)
-def get_surface_fence(fence: np.ndarray, surface: RegularSurface) -> np.ndarray:
+def get_surface_fence(fence: np.ndarray, surface: xtgeo.RegularSurface) -> np.ndarray:
     return surface.get_fence(fence)
