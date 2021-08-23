ac5485f0e5d6bc08e14fa59c5f1ccb22a804b15e
Therese Natterøy <61694854+tnatt@users.noreply.github.com>
2021-08-23 16:38:17 +0200

    Use `ParametersModel` in `InplaceVolumesModel` and `ParameterAnalysis` (#700)

--- webviz_subsurface/plugins/_parameter_analysis/models/parameters_model.py
+++ webviz_subsurface/plugins/_parameter_analysis/models/parameters_model.py
@@ -5,6 +5,7 @@ import pandas as pd
 import plotly.graph_objects as go
 import plotly.express as px
 from dash_table.Format import Format
+from webviz_subsurface._models.parameter_model import ParametersModel as Pmodel
 
 # pylint: disable=too-many-public-methods
 class ParametersModel:
@@ -15,11 +16,13 @@ class ParametersModel:
     def __init__(
         self, dataframe: pd.DataFrame, theme: dict, drop_constants: bool = True
     ) -> None:
-        self._dataframe = dataframe
+        self.pmodel = Pmodel(
+            dataframe=dataframe, drop_constants=drop_constants, keep_numeric_only=True
+        )
+        self._dataframe = self.pmodel.dataframe
+        self._parameters = self.pmodel.parameters
         self.theme = theme
         self.colorway = self.theme.plotly_theme.get("layout", {}).get("colorway", None)
-        self._parameters = []
-        self._prepare_data(drop_constants)
         self._statframe = self._aggregate_ensemble_data(self._dataframe)
         self._statframe_normalized = self._normalize_and_aggregate()
 
@@ -48,61 +51,6 @@ class ParametersModel:
     @property
     def ensembles(self) -> List[str]:
         return list(self.dataframe["ENSEMBLE"].unique())
-
-    def _prepare_data(self, drop_constants):
-        """
-        Different data preparations on the parameters, before storing them as an attribute.
-        Option to drop parameters with constant values. Prefixes on parameters from GEN_KW
-        are removed, in addition parameters with LOG distribution will be kept while the
-        other is dropped.
-        """
-        self._dataframe = self._dataframe.reset_index(drop=True)
-
-        if drop_constants:
-            constant_params = [
-                param
-                for param in [
-                    x for x in self._dataframe.columns if x not in self.REQUIRED_COLUMNS
-                ]
-                if len(self._dataframe[param].unique()) == 1
-            ]
-            self._dataframe = self._dataframe.drop(columns=constant_params)
-
-        # Keep only LOG parameters
-        log_params = [
-            param.replace("LOG10_", "")
-            for param in [
-                x for x in self._dataframe.columns if x not in self.REQUIRED_COLUMNS
-            ]
-            if param.startswith("LOG10_")
-        ]
-        self._dataframe = self._dataframe.drop(columns=log_params)
-        self._dataframe = self._dataframe.rename(
-            columns={
-                col: f"{col} (log)"
-                for col in self._dataframe.columns
-                if col.startswith("LOG10_")
-            }
-        )
-        # Remove prefix on parameter name added by GEN_KW
-        self._dataframe = self._dataframe.rename(
-            columns={
-                col: (col.split(":", 1)[1])
-                for col in self._dataframe.columns
-                if (":" in col and col not in self.REQUIRED_COLUMNS)
-            }
-        )
-        # Drop columns if duplicate names
-        self._dataframe = self._dataframe.loc[:, ~self._dataframe.columns.duplicated()]
-
-        # Only use numeric columns and filter away REQUIRED_COLUMNS
-        self._parameters = [
-            x
-            for x in self._dataframe.columns[
-                [np.issubdtype(dtype, np.number) for dtype in self._dataframe.dtypes]
-            ]
-            if x not in self.REQUIRED_COLUMNS
-        ]
 
     @staticmethod
     def _aggregate_ensemble_data(dframe) -> pd.DataFrame:
