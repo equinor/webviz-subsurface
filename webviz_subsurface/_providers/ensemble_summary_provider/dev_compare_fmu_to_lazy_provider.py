f0b9063680ee2883d5b130a78f97aee6317e7aba
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2021-10-04 22:35:45 +0200

    Extended dev comparison and added resampling perf test

--- webviz_subsurface/_providers/ensemble_summary_provider/dev_compare_fmu_to_lazy_provider.py
+++ webviz_subsurface/_providers/ensemble_summary_provider/dev_compare_fmu_to_lazy_provider.py
@@ -8,7 +8,15 @@ import pandas as pd
 from fmu.ensemble import ScratchEnsemble
 
 from .ensemble_summary_provider import Frequency
+from .ensemble_summary_provider_factory import EnsembleSummaryProvider
 from .ensemble_summary_provider_factory import EnsembleSummaryProviderFactory
+
+# Tolerances inspired by
+#   https://numpy.org/doc/stable/reference/generated/numpy.isclose.html
+# and
+#   https://pandas.pydata.org/docs/reference/api/pandas.testing.assert_frame_equal.html#pandas.testing.assert_frame_equal
+ABS_TOLERANCE = 1e-8
+REL_TOLERANCE = 1e-4
 
 
 def _make_date_column_datetime_object(df: pd.DataFrame) -> pd.DataFrame:
@@ -38,8 +46,13 @@ def _make_date_column_datetime_object(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
-def _load_smry_dataframe_using_fmu(ens_path: str, time_index: str) -> pd.DataFrame:
+def _load_smry_dataframe_using_fmu(ens_path: str, frequency: Frequency) -> pd.DataFrame:
 
+    time_index: str = "raw"
+    if frequency:
+        time_index = frequency.value
+
+    print("time_index", time_index)
     scratch_ensemble = ScratchEnsemble("tempEnsName", paths=ens_path)
     df = scratch_ensemble.load_smry(time_index=time_index)
 
@@ -50,32 +63,22 @@ def _load_smry_dataframe_using_fmu(ens_path: str, time_index: str) -> pd.DataFra
     df[floatcols] = df[floatcols].apply(pd.to_numeric, downcast="float")
     df["REAL"] = df["REAL"].astype("int32")
 
+    # Sort on real, then date to align with provider
+    df.sort_values(by=["REAL", "DATE"], inplace=True)
+
     return df
 
 
-def _compare_fmu_smry_to_lazy_provider(
-    factory: EnsembleSummaryProviderFactory,
-    ens_path: str,
-    frequency: Frequency,
-) -> pd.DataFrame:
+def _compare_fmu_df_to_provider_get_vectors_df(
+    fmu_df: pd.DataFrame, provider: EnsembleSummaryProvider, frequency: Frequency
+) -> None:
 
-    print("## Loading data into DataFrame using FMU...")
-    time_index: str = "raw"
-    if frequency:
-        time_index = frequency.value
-
-    print("time_index", time_index)
-    fmu_df = _load_smry_dataframe_using_fmu(ens_path, time_index)
-    fmu_df.sort_values(by=["REAL", "DATE"], inplace=True)
-    fmu_df.reset_index(inplace=True, drop=True)
+    fmu_df = fmu_df.reset_index(drop=True)
     # print(fmu_df)
 
-    print("## Creating provider...")
-    provider = factory.create_from_arrow_unsmry_lazy(ens_path)
-
-    print("## Getting data from provider...")
+    print("## Getting data for all vectors from provider...")
     provider_df = provider.get_vectors_df(provider.vector_names(), frequency)
-    provider_df.reset_index(inplace=True, drop=True)
+    provider_df = provider_df.reset_index(drop=True)
     # print(provider_df)
 
     print("## Running compare...")
@@ -85,16 +88,72 @@ def _compare_fmu_smry_to_lazy_provider(
         df1_name="fmu_df",
         df2_name="provider_df",
         on_index=True,
-        # abs_tol=0.000001,
-        rel_tol=1e-6,
+        abs_tol=ABS_TOLERANCE,
+        rel_tol=REL_TOLERANCE,
     )
-    print(compare.report())
+    if compare.matches():
+        print("## Comparison ok")
+    else:
+        print("## Comparison DID NOT MATCH")
+        print(compare.report())
 
-    print("\nColumns that are only present in fmu_df:")
-    print(compare.df1_unq_columns())
+        print("## Columns that are only present in fmu_res_df:")
+        print(compare.df1_unq_columns())
 
-    print("\nColumns that are only present in provider_df:")
-    print(compare.df2_unq_columns())
+        print("## Columns that are only present in provider_df:")
+        print(compare.df2_unq_columns())
+
+
+def _compare_fmu_df_to_provider_get_vectors_for_date(
+    fmu_df: pd.DataFrame, provider: EnsembleSummaryProvider, frequency: Frequency
+) -> None:
+
+    all_dates = fmu_df["DATE"].unique()
+    num_dates = len(all_dates)
+    # print(all_dates)
+
+    lookup_date_arr = []
+    lookup_date_arr.append(all_dates[0])
+    lookup_date_arr.append(all_dates[int(num_dates / 3)])
+    lookup_date_arr.append(all_dates[int(num_dates / 2)])
+    lookup_date_arr.append(all_dates[int(2 * num_dates / 3)])
+    lookup_date_arr.append(all_dates[num_dates - 1])
+
+    for lookup_date in lookup_date_arr:
+        fmu_res_df = fmu_df.loc[fmu_df["DATE"] == lookup_date]
+        fmu_res_df = fmu_res_df.drop(columns="DATE")
+        fmu_res_df = fmu_res_df.reset_index(drop=True)
+        # print("## Dumping fmu_res_df:")
+        # print(fmu_res_df)
+
+        provider_df = provider.get_vectors_for_date_df(
+            lookup_date, provider.vector_names()
+        )
+        provider_df = provider_df.reset_index(drop=True)
+        # print("## Dumping provider_df:")
+        # print(provider_df)
+
+        print(f"## Running compare for date {lookup_date} ...")
+        compare = datacompy.Compare(
+            fmu_res_df,
+            provider_df,
+            df1_name="fmu_res_df",
+            df2_name="provider_df",
+            on_index=True,
+            abs_tol=ABS_TOLERANCE,
+            rel_tol=REL_TOLERANCE,
+        )
+        if compare.matches():
+            print(f"## Comparison ok for date {lookup_date}")
+        else:
+            print(f"## Comparison DID NOT MATCH for date {lookup_date}")
+            print(compare.report())
+
+            print("## Columns that are only present in fmu_res_df:")
+            print(compare.df1_unq_columns())
+
+            print("## Columns that are only present in provider_df:")
+            print(compare.df2_unq_columns())
 
 
 def main() -> None:
@@ -111,25 +170,35 @@ def main() -> None:
 
     root_storage_dir = Path("/home/sigurdp/buf/webviz_storage_dir")
 
-    # ensemble_path = "../webviz-subsurface-testdata/01_drogon_ahm/realization-*/iter-0"
-    ensemble_path = (
-        "../webviz-subsurface-testdata/01_drogon_design/realization-*/iter-0"
-    )
+    ensemble_path = "../webviz-subsurface-testdata/01_drogon_ahm/realization-*/iter-0"
+    # ensemble_path = (
+    #     "../webviz-subsurface-testdata/01_drogon_design/realization-*/iter-0"
+    # )
     # ensemble_path = (
     #     "/home/sigurdp/webviz_testdata/reek_history_match_large/realization-*/iter-0"
     # )
 
+    # frequency = Frequency.DAILY
     frequency = Frequency.WEEKLY
 
     print()
     print("## root_storage_dir:", root_storage_dir)
     print("## ensemble_path:", ensemble_path)
     print("## frequency:", frequency)
+    print()
 
+    print("## Creating provider...")
     factory = EnsembleSummaryProviderFactory(root_storage_dir)
+    provider = factory.create_from_arrow_unsmry_lazy(ensemble_path)
 
-    print("## Loading data and comparing...")
-    _compare_fmu_smry_to_lazy_provider(factory, ensemble_path, frequency)
+    print("## Loading data into DataFrame using FMU...")
+    fmu_df = _load_smry_dataframe_using_fmu(ensemble_path, frequency)
+
+    print("## Comparing get_vectors...")
+    _compare_fmu_df_to_provider_get_vectors_df(fmu_df, provider, frequency)
+
+    print("## Comparing get_vectors_for date...")
+    _compare_fmu_df_to_provider_get_vectors_for_date(fmu_df, provider, frequency)
 
     print("## done")
 
