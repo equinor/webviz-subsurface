5b9deaef96ebf832129e67113f820c1eb46a370d
Øyvind Lind-Johansen (CCI RPT RES1) <olind@st-linrgsn232.st.statoil.no>
2021-10-05 17:36:55 +0200

    add options layout

---
+++ webviz_subsurface/plugins/_group_tree/views/options_view.py
@@ -0,0 +1,43 @@
+from typing import Callable
+
+import webviz_core_components as wcc
+from dash import html
+
+
+def options_layout(get_uuid: Callable) -> wcc.Selectors:
+    """The filters part of the menu"""
+    options_uuid = get_uuid("options")
+    return wcc.Selectors(
+        id=get_uuid("options_layout"),
+        label="Options",
+        children=[
+            html.Div(
+                id={"id": options_uuid, "element": "statistical_options"},
+                children=[
+                    wcc.RadioItems(
+                        id={"id": options_uuid, "element": "statistical_option"},
+                        options=[
+                            {"label": "Mean", "value": "mean"},
+                            {"label": "P50/Median", "value": "p50"},
+                            {"label": "P10", "value": "p10"},
+                            {"label": "P90", "value": "p90"},
+                            {"label": "Max", "value": "max"},
+                            {"label": "Min", "value": "min"},
+                        ],
+                    )
+                ],
+            ),
+            html.Div(
+                id={"id": options_uuid, "element": "single_real_options"},
+                children=[
+                    wcc.Dropdown(
+                        label="Realization",
+                        id={"id": options_uuid, "element": "realization"},
+                        options=[],
+                        value=None,
+                        multi=False,
+                    )
+                ],
+            ),
+        ],
+    )
