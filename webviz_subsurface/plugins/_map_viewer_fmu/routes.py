fad761bca670529caea7cf6df22fbb41cbc1060d
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2021-10-23 11:18:08 +0200

    Replace some strings with enums

--- webviz_subsurface/plugins/_map_viewer_fmu/routes.py
+++ webviz_subsurface/plugins/_map_viewer_fmu/routes.py
@@ -9,15 +9,15 @@ from webviz_config.common_cache import CACHE
 import webviz_subsurface
 
 from .models import SurfaceSetModel
-from .utils.surface_utils import surface_spec_from_url, surface_to_rgba
+from .utils.surface_utils import surface_context_from_url, surface_to_rgba
 
 
 def deckgl_map_routes(app: Dash, surface_set_models: List[SurfaceSetModel]) -> None:
     @CACHE.memoize(timeout=CACHE.TIMEOUT)
     def _send_surface_as_png(hash: str):
-        surface_spec = surface_spec_from_url(hash)
-        ensemble = surface_spec.ensemble
-        surface = surface_set_models[ensemble].get_surface(surface_spec)
+        surface_context = surface_context_from_url(hash)
+        ensemble = surface_context.ensemble
+        surface = surface_set_models[ensemble].get_surface(surface_context)
         img_stream = surface_to_rgba(surface).read()
         return send_file(BytesIO(img_stream), mimetype="image/png")
 
