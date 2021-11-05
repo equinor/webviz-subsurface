d581bbe3e5039222d554abd8a9fd32f729c90788
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2021-11-05 15:26:26 +0100

    Add verification of consistent vector metadata for provider set

--- webviz_subsurface/plugins/_simulation_time_series/provider_set.py
+++ webviz_subsurface/plugins/_simulation_time_series/provider_set.py
@@ -8,7 +8,7 @@ from webviz_subsurface._providers import (
 
 
 # TODO: Consider if "ensemble" should be a part of names in class. Both class name,
-# funciton names and attribute names
+# function names and attribute names
 class ProviderSet:
     """
     Class to create a set of ensemble summary providers with unique names
@@ -18,11 +18,48 @@ class ProviderSet:
 
     def __init__(self, provider_dict: Dict[str, EnsembleSummaryProvider]) -> None:
         self._provider_dict = provider_dict.copy()
-        self._all_vector_names: List[
-            str
-        ] = self._create_union_of_vector_names_from_providers(
+        self._all_vector_names = self._create_union_of_vector_names_from_providers(
             list(self._provider_dict.values())
         )
+
+        # Verify consistent vector metadata across providers
+        self._verify_consistent_vector_metadata()
+
+    def _verify_consistent_vector_metadata(self) -> None:
+        """
+        Verify that vector metadata is consistent across providers, raise exception
+        if inconsistency occur.
+
+        TODO: Improve print of inconsistent metadata info - store all inconsistencies
+        and print, do not raise ValueError on first
+        """
+
+        # Iterate through all vector names for provider set
+        for vector_name in self._all_vector_names:
+            # Store provider name and retrieved vector metadata for specific vector name
+            vector_provider_metadata_dict: Dict[str, Optional[Dict[str, Any]]] = {}
+
+            # Retrieve vector metadata from providers
+            for name, provider in self._provider_dict.items():
+                if vector_name in provider.vector_names():
+                    vector_provider_metadata_dict[name] = provider.vector_metadata(
+                        vector_name
+                    )
+            if vector_provider_metadata_dict:
+                validator_provider, metadata_validator = list(
+                    vector_provider_metadata_dict.items()
+                )[0]
+                for (
+                    provider_name,
+                    vector_metadata,
+                ) in vector_provider_metadata_dict.items():
+                    if vector_metadata != metadata_validator:
+
+                        raise ValueError(
+                            f'Inconsistent vector metadata for vector "{vector_name}"'
+                            f' between provider "{validator_provider}" and provider '
+                            f'{provider_name}"'
+                        )
 
     @staticmethod
     def _create_union_of_vector_names_from_providers(
@@ -54,10 +91,10 @@ class ProviderSet:
         return self._all_vector_names
 
     def vector_metadata(self, vector: str) -> Optional[Dict[str, Any]]:
-        """Get vector metadata from first occurence among providers,
+        """Get vector metadata from first occurrence among providers,
 
         `return:`
-        Vector metadata dict from first occurence among providers, None if not existing
+        Vector metadata dict from first occurrence among providers, None if not existing
         """
         metadata: Optional[Dict[str, Any]] = next(
             (
