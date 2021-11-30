c617bb70979fd06229112301075f041e71e83896
Øyvind Lind-Johansen <olind@equinor.com>
2021-11-30 10:25:21 +0100

    new tab for correlations and class for layout element names

--- webviz_subsurface/plugins/_rft_plotter/_layout.py
+++ webviz_subsurface/plugins/_rft_plotter/_layout.py
@@ -6,6 +6,46 @@ from dash import html
 from ._business_logic import RftPlotterDataModel
 
 
+# pylint: disable = too-few-public-methods
+class LayoutElements:
+    MAP = "map-wrapper"
+    FORMATIONS_GRAPH = "formations-graph-wrapper"
+    MISFITPLOT_GRAPH = "misfit-graph-wrapper"
+    CROSSPLOT_GRAPH = "crossplot-graph-wrapper"
+    ERRORPLOT_GRAPH = "errorplot-graph-wrapper"
+    FORMATIONS_ENSEMBLE = "formations-ensemble"
+    FORMATIONS_WELL = "formations-well"
+    FORMATIONS_DATE = "formations-date"
+    FORMATIONS_LINETYPE = "formations-linetype"
+    FORMATIONS_DEPTHOPTION = "formations-depthoption"
+    MAP_ENSEMBLE = "map-ensemble"
+    MAP_SIZE_BY = "map-size-by"
+    MAP_COLOR_BY = "map-color-by"
+    MAP_DATE_RANGE = "map-date-range"
+    FILTER_ENSEMBLES = {
+        "misfitplot": "ensembles-misfit",
+        "crossplot": "ensembles-crossplot",
+        "errorplot": "ensembles-errorplot",
+    }
+    FILTER_WELLS = {
+        "misfitplot": "well-misfit",
+        "crossplot": "well-crossplot",
+        "errorplot": "well-errorplot",
+    }
+    FILTER_ZONES = {
+        "misfitplot": "zones-misfit",
+        "crossplot": "zones-crossplot",
+        "errorplot": "zones-errorplot",
+    }
+    FILTER_DATES = {
+        "misfitplot": "dates-misfit",
+        "crossplot": "dates-crossplot",
+        "errorplot": "dates-errorplot",
+    }
+    CROSSPLOT_COLOR_BY = "crossplot-color-by"
+    CROSSPLOT_SIZE_BY = "crossplot-size-by"
+
+
 def main_layout(get_uuid: Callable, datamodel: RftPlotterDataModel) -> wcc.Tabs:
     return wcc.Tabs(
         children=[
@@ -25,14 +65,14 @@ def main_layout(get_uuid: Callable, datamodel: RftPlotterDataModel) -> wcc.Tabs:
                                 style={"flex": 3, "height": "87vh"},
                                 color="white",
                                 highlight=False,
-                                id=get_uuid("map"),
+                                id=get_uuid(LayoutElements.MAP),
                                 children=[],
                             ),
                             wcc.Frame(
                                 style={"flex": 3, "height": "87vh"},
                                 color="white",
                                 highlight=False,
-                                id=get_uuid("formations-graph-wrapper"),
+                                id=get_uuid(LayoutElements.FORMATIONS_GRAPH),
                                 children=[],
                             ),
                         ]
@@ -54,7 +94,7 @@ def main_layout(get_uuid: Callable, datamodel: RftPlotterDataModel) -> wcc.Tabs:
                                 style={"flex": 6, "height": "87vh"},
                                 color="white",
                                 highlight=False,
-                                id=get_uuid("misfit-graph-wrapper"),
+                                id=get_uuid(LayoutElements.MISFITPLOT_GRAPH),
                                 children=[],
                             ),
                         ]
@@ -78,7 +118,9 @@ def main_layout(get_uuid: Callable, datamodel: RftPlotterDataModel) -> wcc.Tabs:
                                 color="white",
                                 highlight=False,
                                 children=[
-                                    html.Div(id=get_uuid("crossplot-graph-wrapper")),
+                                    html.Div(
+                                        id=get_uuid(LayoutElements.CROSSPLOT_GRAPH)
+                                    ),
                                 ],
                             ),
                         ],
@@ -100,14 +142,69 @@ def main_layout(get_uuid: Callable, datamodel: RftPlotterDataModel) -> wcc.Tabs:
                                 color="white",
                                 highlight=False,
                                 style={"flex": 6, "height": "87vh"},
-                                id=get_uuid("errorplot-graph-wrapper"),
+                                id=get_uuid(LayoutElements.ERRORPLOT_GRAPH),
                                 children=[],
                             ),
                         ],
                     ),
                 ],
             ),
+            wcc.Tab(
+                label="RFT correlations",
+                children=correlations_view(get_uuid=get_uuid, datamodel=datamodel),
+            ),
         ],
+    )
+
+
+def correlations_view(
+    get_uuid: Callable, datamodel: RftPlotterDataModel
+) -> wcc.FlexBox:
+    return wcc.FlexBox(
+        children=[
+            wcc.FlexColumn(flex=1, children=[]),
+            wcc.FlexColumn(
+                flex=4,
+                children=wcc.FlexBox(
+                    children=[
+                        wcc.FlexColumn(
+                            flex=2,
+                            children=[
+                                wcc.Frame(
+                                    style={"height": "38.5vh"},
+                                    color="white",
+                                    highlight=False,
+                                    children=[],  # timeseries_view(get_uuid=get_uuid),
+                                ),
+                                wcc.Frame(
+                                    style={"height": "38.5vh"},
+                                    color="white",
+                                    highlight=False,
+                                    children=[],
+                                ),
+                            ],
+                        ),
+                        wcc.FlexColumn(
+                            flex=2,
+                            children=[
+                                wcc.Frame(
+                                    color="white",
+                                    highlight=False,
+                                    style={"height": "38.5vh"},
+                                    children=[],
+                                ),
+                                wcc.Frame(
+                                    color="white",
+                                    highlight=False,
+                                    style={"height": "38.5vh"},
+                                    children=[],
+                                ),
+                            ],
+                        ),
+                    ],
+                ),
+            ),
+        ]
     )
 
 
@@ -122,7 +219,7 @@ def formation_plot_selectors(
         children=[
             wcc.Dropdown(
                 label="Ensemble",
-                id=get_uuid("ensemble"),
+                id=get_uuid(LayoutElements.FORMATIONS_ENSEMBLE),
                 options=[{"label": ens, "value": ens} for ens in ensembles],
                 value=ensembles[0],
                 multi=True,
@@ -130,14 +227,14 @@ def formation_plot_selectors(
             ),
             wcc.Dropdown(
                 label="Well",
-                id=get_uuid("well"),
+                id=get_uuid(LayoutElements.FORMATIONS_WELL),
                 options=[{"label": well, "value": well} for well in well_names],
                 value=well_names[0],
                 clearable=False,
             ),
             wcc.Dropdown(
                 label="Date",
-                id=get_uuid("date"),
+                id=get_uuid(LayoutElements.FORMATIONS_DATE),
                 options=[
                     {"label": date, "value": date}
                     for date in date_in_well(well_names[0])
@@ -147,7 +244,7 @@ def formation_plot_selectors(
             ),
             wcc.RadioItems(
                 label="Plot simulations as",
-                id=get_uuid("linetype"),
+                id=get_uuid(LayoutElements.FORMATIONS_LINETYPE),
                 options=[
                     {
                         "label": "Realization lines",
@@ -162,7 +259,7 @@ def formation_plot_selectors(
             ),
             wcc.RadioItems(
                 label="Depth option",
-                id=get_uuid("depth_option"),
+                id=get_uuid(LayoutElements.FORMATIONS_DEPTHOPTION),
                 options=[
                     {
                         "label": "TVD",
@@ -188,14 +285,14 @@ def map_plot_selectors(
         children=[
             wcc.Dropdown(
                 label="Ensemble",
-                id=get_uuid("map_ensemble"),
+                id=get_uuid(LayoutElements.MAP_ENSEMBLE),
                 options=[{"label": ens, "value": ens} for ens in ensembles],
                 value=ensembles[0],
                 clearable=False,
             ),
             wcc.Dropdown(
                 label="Size points by",
-                id=get_uuid("map_size"),
+                id=get_uuid(LayoutElements.MAP_SIZE_BY),
                 options=[
                     {
                         "label": "Standard Deviation",
@@ -211,7 +308,7 @@ def map_plot_selectors(
             ),
             wcc.Dropdown(
                 label="Color points by",
-                id=get_uuid("map_color"),
+                id=get_uuid(LayoutElements.MAP_COLOR_BY),
                 options=[
                     {
                         "label": "Misfit",
@@ -231,7 +328,7 @@ def map_plot_selectors(
             ),
             wcc.RangeSlider(
                 label="Filter date range",
-                id=get_uuid("map_date"),
+                id=get_uuid(LayoutElements.MAP_DATE_RANGE),
                 min=datamodel.ertdatadf["DATE_IDX"].min(),
                 max=datamodel.ertdatadf["DATE_IDX"].max(),
                 value=[
@@ -258,7 +355,7 @@ def filter_layout(
             wcc.SelectWithLabel(
                 label="Ensembles",
                 size=min(4, len(ensembles)),
-                id=get_uuid(f"ensemble-{tab}"),
+                id=get_uuid(LayoutElements.FILTER_ENSEMBLES[tab]),
                 options=[{"label": name, "value": name} for name in ensembles],
                 value=ensembles,
                 multi=True,
@@ -266,7 +363,7 @@ def filter_layout(
             wcc.SelectWithLabel(
                 label="Wells",
                 size=min(20, len(well_names)),
-                id=get_uuid(f"well-{tab}"),
+                id=get_uuid(LayoutElements.FILTER_WELLS[tab]),
                 options=[{"label": name, "value": name} for name in well_names],
                 value=well_names,
                 multi=True,
@@ -274,7 +371,7 @@ def filter_layout(
             wcc.SelectWithLabel(
                 label="Zones",
                 size=min(10, len(zone_names)),
-                id=get_uuid(f"zone-{tab}"),
+                id=get_uuid(LayoutElements.FILTER_ZONES[tab]),
                 options=[{"label": name, "value": name} for name in zone_names],
                 value=zone_names,
                 multi=True,
@@ -282,7 +379,7 @@ def filter_layout(
             wcc.SelectWithLabel(
                 label="Dates",
                 size=min(10, len(dates)),
-                id=get_uuid(f"date-{tab}"),
+                id=get_uuid(LayoutElements.FILTER_DATES[tab]),
                 options=[{"label": name, "value": name} for name in dates],
                 value=dates,
                 multi=True,
@@ -297,7 +394,7 @@ def size_color_layout(get_uuid: Callable) -> List[html.Div]:
         children=[
             wcc.Dropdown(
                 label="Color by",
-                id=get_uuid("crossplot_color"),
+                id=get_uuid(LayoutElements.CROSSPLOT_COLOR_BY),
                 options=[
                     {
                         "label": "Misfit",
@@ -313,7 +410,7 @@ def size_color_layout(get_uuid: Callable) -> List[html.Div]:
             ),
             wcc.Dropdown(
                 label="Size by",
-                id=get_uuid("crossplot_size"),
+                id=get_uuid(LayoutElements.CROSSPLOT_SIZE_BY),
                 options=[
                     {
                         "label": "Standard Deviation",
