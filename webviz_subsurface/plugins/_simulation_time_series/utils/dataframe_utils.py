88fd605d383357600b56c5f21c37861a6c27445d
Jørgen Herje <jorgen.herje@ceetronsolutions.com>
2022-02-10 15:58:58 +0100

    Fix based on review

--- webviz_subsurface/plugins/_simulation_time_series/utils/dataframe_utils.py
+++ webviz_subsurface/plugins/_simulation_time_series/utils/dataframe_utils.py
@@ -29,8 +29,8 @@ def create_relative_to_date_df(
 
     NOTE:
     - This function iterates over realization group in input dataframe
-    - For-loop makes it possible to get realization not present in _relative_date_df, if
-    realization is not present in _relative_date_df the realization is excluded output.
+    - For-loop makes it possible to get realization not present in relative_date_df, if
+    realization is not present in relative_date_df the realization is excluded output.
     """
 
     assert_date_column_is_datetime_object(df)
@@ -42,27 +42,27 @@ def create_relative_to_date_df(
     _columns = {name: pd.Series(dtype=df.dtypes[name]) for name in df.columns}
     output_df = pd.DataFrame(_columns)
 
-    _relative_date_df: pd.DataFrame = df.loc[df["DATE"] == relative_date].drop(
+    relative_date_df: pd.DataFrame = df.loc[df["DATE"] == relative_date].drop(
         columns=["DATE"]
     )
-    if _relative_date_df.empty:
+    if relative_date_df.empty:
         # Dataframe with columns, but no rows
         return output_df
 
     vectors = [elm for elm in df.columns if elm not in ("DATE", "REAL")]
 
-    # NOTE: This for-loop makes it possible to get real not represented in _relative_date_df!
-    for _real, _df in df.groupby("REAL"):
-        _relative_date_data = _relative_date_df.loc[
-            _relative_date_df["REAL"] == _real
+    # NOTE: This for-loop makes it possible to get real not represented in relative_date_df!
+    for real, real_df in df.groupby("REAL"):
+        relative_date_data = relative_date_df.loc[
+            relative_date_df["REAL"] == real
         ].drop(columns=["REAL"])
 
-        # If realization does not exist in _relative_date_df
-        if _relative_date_data.empty:
+        # If realization does not exist in relative_date_df
+        if relative_date_data.empty:
             continue
 
-        _df[vectors] = _df[vectors].sub(_relative_date_data.iloc[0], axis=1)
-        output_df = pd.concat([output_df, _df], ignore_index=True)
+        real_df[vectors] = real_df[vectors].sub(relative_date_data.iloc[0], axis=1)
+        output_df = pd.concat([output_df, real_df], ignore_index=True)
 
     make_date_column_datetime_object(output_df)
     return output_df
