d3aeebfb8b24ea73d00a3e0eccab3ef41fea2605
Øyvind Lind-Johansen <olind@equinor.com>
2022-02-13 14:59:23 +0100

    well control layout in separate file

--- webviz_subsurface/plugins/_well_analysis/_layout/layout.py
+++ webviz_subsurface/plugins/_well_analysis/_layout/layout.py
@@ -4,19 +4,18 @@ import webviz_core_components as wcc
 from dash import html
 
 from .._ensemble_data import EnsembleData
-
+from .well_control_layout import well_control_tab
 
 # pylint: disable = too-few-public-methods
 class LayoutElements:
     WELL_OVERVIEW_GRAPH = "well-overview-graph"
-    WELL_CONTROL_GRAPH = "well-control-graph"
 
 
 def main_layout(get_uuid: Callable, data_models: Dict[str, EnsembleData]) -> wcc.Tabs:
     """Main layout"""
     tabs = [
         wcc.Tab(label="Well Overview", children=well_overview_tab(get_uuid)),
-        wcc.Tab(label="Well Control", children=well_control_tab(get_uuid)),
+        wcc.Tab(label="Well Control", children=well_control_tab(get_uuid, data_models)),
     ]
 
     return wcc.Tabs(children=tabs)
@@ -35,24 +34,6 @@ def well_overview_tab(get_uuid: Callable) -> wcc.FlexBox:
                 color="white",
                 highlight=False,
                 id=get_uuid(LayoutElements.WELL_OVERVIEW_GRAPH),
-                children=[],
-            ),
-        ]
-    )
-
-
-def well_control_tab(get_uuid: Callable) -> wcc.FlexBox:
-    return wcc.FlexBox(
-        children=[
-            wcc.Frame(
-                style={"flex": 1, "height": "87vh"},
-                children=[],
-            ),
-            wcc.Frame(
-                style={"flex": 4, "height": "87vh"},
-                color="white",
-                highlight=False,
-                id=get_uuid(LayoutElements.WELL_CONTROL_GRAPH),
                 children=[],
             ),
         ]
---
+++ webviz_subsurface/plugins/_well_analysis/_layout/well_control_layout.py
@@ -0,0 +1,121 @@
+from typing import Callable, Dict, List
+
+import webviz_core_components as wcc
+from dash import html
+
+from .._ensemble_data import EnsembleData
+
+# pylint: disable = too-few-public-methods
+class WellControlLayoutElements:
+    GRAPH = "well-control-graph"
+    ENSEMBLE = "well-control-ensemble"
+    WELL = "well-control-well"
+    INCLUDE_BHP = "well-control-include-bhp"
+    MEAN_OR_REAL = "well-control-mean-or-real"
+    SINGLE_REAL_OPTIONS = "well-control-single-real-options"
+    REAL = "well-control-real"
+    CTRLMODE_BAR = "well-control-ctrlmode-bar"
+    SHARED_XAXIS = "well-control-shared-xaxis"
+
+
+def well_control_tab(
+    get_uuid: Callable, data_models: Dict[str, EnsembleData]
+) -> wcc.FlexBox:
+    return wcc.FlexBox(
+        children=[
+            wcc.Frame(
+                style={"flex": 1, "height": "87vh"},
+                children=controls(get_uuid, data_models),
+            ),
+            wcc.Frame(
+                style={"flex": 4, "height": "87vh"},
+                color="white",
+                highlight=False,
+                id=get_uuid(WellControlLayoutElements.GRAPH),
+                children=[],
+            ),
+        ]
+    )
+
+
+def controls(get_uuid: Callable, data_models: Dict[str, EnsembleData]) -> wcc.Frame:
+    return wcc.Frame(
+        style={"height": "87vh"},
+        children=[
+            wcc.Selectors(
+                label="Selectors",
+                children=[
+                    wcc.Dropdown(
+                        label="Ensemble",
+                        id=WellControlLayoutElements.ENSEMBLE,
+                        # options=[{"label": col, "value": col} for col in ensembles],
+                        # value=ensembles[0],
+                        multi=False,
+                    ),
+                    wcc.Dropdown(
+                        label="Well",
+                        id=WellControlLayoutElements.WELL,
+                        options=[],
+                        value=None,
+                        multi=False,
+                    ),
+                ],
+            ),
+            wcc.Selectors(
+                label="Pressure Plot Options",
+                children=[
+                    wcc.Checklist(
+                        id=WellControlLayoutElements.INCLUDE_BHP,
+                        options=[{"label": "Include BHP", "value": "include_bhp"}],
+                        value=["include_bhp"],
+                    ),
+                    wcc.RadioItems(
+                        label="Mean or realization",
+                        id=WellControlLayoutElements.MEAN_OR_REAL,
+                        options=[
+                            {
+                                "label": "Mean of producing real.",
+                                "value": "plot_mean",
+                            },
+                            {
+                                "label": "Single realization",
+                                "value": "single_real",
+                            },
+                        ],
+                        value="plot_mean",
+                    ),
+                    html.Div(
+                        id=WellControlLayoutElements.SINGLE_REAL_OPTIONS,
+                        children=[
+                            wcc.Dropdown(
+                                id=WellControlLayoutElements.REAL,
+                                options=[],
+                                value=None,
+                                multi=False,
+                            ),
+                            wcc.Checklist(
+                                id=WellControlLayoutElements.CTRLMODE_BAR,
+                                options=[
+                                    {
+                                        "label": "Display ctrl mode bar",
+                                        "value": "ctrlmode_bar",
+                                    }
+                                ],
+                                value=["ctrlmode_bar"],
+                            ),
+                        ],
+                    ),
+                ],
+            ),
+            wcc.Selectors(
+                label="⚙️ Settings",
+                children=[
+                    wcc.Checklist(
+                        id=get_uuid(WellControlLayoutElements.SHARED_XAXIS),
+                        options=[{"label": "Shared x-axis", "value": "shared_xaxes"}],
+                        value=["shared_xaxes"],
+                    ),
+                ],
+            ),
+        ],
+    )
