cf74a02211320bd6b1ddef2f5e0725b7500e45e6
Øyvind Lind-Johansen <olind@equinor.com>
2022-02-13 08:12:34 +0100

    main layout for well control tab

--- webviz_subsurface/plugins/_well_analysis/_layout/layout.py
+++ webviz_subsurface/plugins/_well_analysis/_layout/layout.py
@@ -9,13 +9,14 @@ from .._ensemble_data import EnsembleData
 # pylint: disable = too-few-public-methods
 class LayoutElements:
     WELL_OVERVIEW_GRAPH = "well-overview-graph"
+    WELL_CONTROL_GRAPH = "well-control-graph"
 
 
 def main_layout(get_uuid: Callable, data_models: Dict[str, EnsembleData]) -> wcc.Tabs:
     """Main layout"""
     tabs = [
         wcc.Tab(label="Well Overview", children=well_overview_tab(get_uuid)),
-        wcc.Tab(label="Well Control", children="Not implemented"),
+        wcc.Tab(label="Well Control", children=well_control_tab(get_uuid)),
     ]
 
     return wcc.Tabs(children=tabs)
@@ -34,6 +35,24 @@ def well_overview_tab(get_uuid: Callable) -> wcc.FlexBox:
                 color="white",
                 highlight=False,
                 id=get_uuid(LayoutElements.WELL_OVERVIEW_GRAPH),
+                children=[],
+            ),
+        ]
+    )
+
+
+def well_control_tab(get_uuid: Callable) -> wcc.FlexBox:
+    return wcc.FlexBox(
+        children=[
+            wcc.Frame(
+                style={"flex": 1, "height": "87vh"},
+                children=[],
+            ),
+            wcc.Frame(
+                style={"flex": 4, "height": "87vh"},
+                color="white",
+                highlight=False,
+                id=get_uuid(LayoutElements.WELL_CONTROL_GRAPH),
                 children=[],
             ),
         ]
