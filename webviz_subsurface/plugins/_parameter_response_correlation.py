b09884d8abac086633c159f31283cb97412de72f
Asgeir Nyvoll <47146384+asnyv@users.noreply.github.com>
2022-04-21 12:15:42 +0200

    Fixes issues with just one parameter and for aggregated data in ParameterResponseCorrelation (#1001)

--- webviz_subsurface/plugins/_parameter_response_correlation.py
+++ webviz_subsurface/plugins/_parameter_response_correlation.py
@@ -308,10 +308,10 @@ folder, to avoid risk of not extracting the right data.
                     id=self.uuid("max-params"),
                     min=1,
                     max=max_params,
-                    marks={"1": 1, str(max_params): max_params},
+                    marks={1: "1", max_params: str(max_params)},
                     value=max_params,
                 ),
-                style={"margin-top": "10px"},
+                style={"marginTop": "10px"},
             ),
         ]
 
@@ -330,9 +330,15 @@ folder, to avoid risk of not extracting the right data.
                                 label="Controls", children=self.control_layout
                             )
                         ]
-                        + [wcc.Selectors(label="Filters", children=self.filter_layout)]
-                        if self.response_filters
-                        else [],
+                        + (
+                            [
+                                wcc.Selectors(
+                                    label="Filters", children=self.filter_layout
+                                )
+                            ]
+                            if self.response_filters
+                            else []
+                        ),
                     ),
                 ),
                 wcc.FlexColumn(
