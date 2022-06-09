645d899de59b9bf7014a0cbd9985644e09a033f2
Sigurd Pettersen <sigurd.pettersen@ceetronsolutions.com>
2022-06-09 16:06:32 +0200

    Commented out some noisy logging

--- webviz_subsurface/_providers/ensemble_surface_provider/_surface_to_image.py
+++ webviz_subsurface/_providers/ensemble_surface_provider/_surface_to_image.py
@@ -90,16 +90,16 @@ def surface_to_png_bytes_optimized(surface: xtgeo.RegularSurface) -> bytes:
     surf_values_ma: np.ma.MaskedArray = surface.values
 
     surf_values_ma = np.flip(surf_values_ma.transpose(), axis=0)  # type: ignore
-    LOGGER.debug(f"flip/transpose: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"flip/transpose: {timer.lap_s():.2f}s")
 
     # This will be a flat bool array with true for all valid entries
     valid_arr = np.invert(np.ma.getmaskarray(surf_values_ma).flatten())
-    LOGGER.debug(f"get valid_arr: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"get valid_arr: {timer.lap_s():.2f}s")
 
     shape = surf_values_ma.shape
     min_val = surf_values_ma.min()
     max_val = surf_values_ma.max()
-    LOGGER.debug(f"minmax: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"minmax: {timer.lap_s():.2f}s")
 
     if min_val == 0.0 and max_val == 0.0:
         scale_factor = 1.0
@@ -112,10 +112,10 @@ def surface_to_png_bytes_optimized(surface: xtgeo.RegularSurface) -> bytes:
     # Get a NON-masked array with all undefined entries filled with 0
     scaled_values = scaled_values_ma.filled(0)
 
-    LOGGER.debug(f"scale and fill: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"scale and fill: {timer.lap_s():.2f}s")
 
     val_arr = scaled_values.astype(np.uint32).ravel()
-    LOGGER.debug(f"cast and flatten: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"cast and flatten: {timer.lap_s():.2f}s")
 
     val = val_arr.view(dtype=np.uint8)
     rgba_arr = np.empty(4 * len(val_arr), dtype=np.uint8)
@@ -124,22 +124,22 @@ def surface_to_png_bytes_optimized(surface: xtgeo.RegularSurface) -> bytes:
     rgba_arr[2::4] = val[0::4]
     rgba_arr[3::4] = np.multiply(valid_arr, 255).astype(np.uint8)
 
-    LOGGER.debug(f"rgba combine: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"rgba combine: {timer.lap_s():.2f}s")
 
     # Back to 2d shape + 1 dimension for the rgba values.
     rgba_arr_reshaped = rgba_arr.reshape((shape[0], shape[1], 4))
 
     image = Image.fromarray(rgba_arr_reshaped, "RGBA")
-    LOGGER.debug(f"create: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"create: {timer.lap_s():.2f}s")
 
     byte_io = io.BytesIO()
     image.save(byte_io, format="png", compress_level=1)
-    LOGGER.debug(f"save png to bytes: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"save png to bytes: {timer.lap_s():.2f}s")
 
     byte_io.seek(0)
     ret_bytes = byte_io.read()
-    LOGGER.debug(f"read bytes: {timer.lap_s():.2f}s")
+    # LOGGER.debug(f"read bytes: {timer.lap_s():.2f}s")
 
-    LOGGER.debug(f"Total time: {timer.elapsed_s():.2f}s")
+    # LOGGER.debug(f"Total time: {timer.elapsed_s():.2f}s")
 
     return ret_bytes
