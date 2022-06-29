81d79a8a5038fb608db0f79080084e7b5fc8c31b
Øyvind Lind-Johansen <olind@equinor.com>
2022-06-29 11:03:59 +0200

    bugfix

--- tests/unit_tests/provider_tests/test_ensemble_table_provider.py
+++ tests/unit_tests/provider_tests/test_ensemble_table_provider.py
@@ -151,6 +151,6 @@ def test_create_provider_set_from_aggregated_csv_file(tmp_path: Path) -> None:
     ] = factory.create_provider_set_from_aggregated_csv_file("tests/data/volumes.csv")
     assert set(provider_set.keys()) == {"iter-0", "iter-1"}
 
-    for ens_name, provider in provider_set.items():
+    for _, provider in provider_set.items():
         valdf = provider.get_column_data(provider.column_names())
         assert valdf["REAL"].unique() == [0, 1]
