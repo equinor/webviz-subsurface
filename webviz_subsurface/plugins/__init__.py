b8ca77ced4ada4285ce6c072e50bfe21ab30c89c
Eirik Sundby Håland (OG SUB RPE) <ehala@be-linrgsn025.be.statoil.no>
2022-06-30 13:13:02 +0200

    import PvtPlotter class to the init file of plugins

--- webviz_subsurface/plugins/__init__.py
+++ webviz_subsurface/plugins/__init__.py
@@ -31,6 +31,7 @@ from ._inplace_volumes_onebyone import InplaceVolumesOneByOne
 from ._line_plotter_fmu.line_plotter_fmu import LinePlotterFMU
 from ._map_viewer_fmu import MapViewerFMU
 from ._morris_plot import MorrisPlot
+from ._new_pvt_plot import PvtPlotter
 from ._parameter_analysis import ParameterAnalysis
 from ._parameter_correlation import ParameterCorrelation
 from ._parameter_distribution import ParameterDistribution
