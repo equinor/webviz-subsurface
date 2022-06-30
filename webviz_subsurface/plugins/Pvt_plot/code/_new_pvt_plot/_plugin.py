6ef43d7f426275ae353e1f153e2c417c8e983575
Kasper Seglem (OG SUB RPE) <kseg@be-linrgsn101.be.statoil.no>
2022-06-30 09:58:21 +0200

    Add error messages

--- webviz_subsurface/plugins/Pvt_plot/code/_new_pvt_plot/_plugin.py
+++ webviz_subsurface/plugins/Pvt_plot/code/_new_pvt_plot/_plugin.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 from dash.development.base_component import Component
 import pandas as pd
+from pandas.errors import ParserError
+from pandas.errors import EmptyDataError
 from webviz_config import WebvizPluginABC
 
 from ._error import error
@@ -10,30 +12,53 @@ from .views import PvtView
 from ._pliginIds import PluginIds
 from .shared_settings import Filter
 
+
 class PvtPlotter(WebvizPluginABC):
-    def __init__(self, pvt_relative_file_path: str = None, ) -> None:
+    def __init__(
+        self,
+        pvt_relative_file_path: str = None,
+    ) -> None:
         super().__init__()
 
-        #Error messages
-
+        # Error messages
         self.error_message = ""
 
         try:
             self.pvt_df = pd.read_csv(pvt_relative_file_path)
+
         except PermissionError:
             self.error_message = f"Access to file '{pvt_relative_file_path}' denied"
             return
-        #add more error messages
 
-        self.add_store(PluginIds.Stores.SELECTED_PHASE, WebvizPluginABC.StorageType.SESSION)
-        self.add_store(PluginIds.Stores.SELECTED_COLOR, WebvizPluginABC.StorageType.SESSION)
+        except FileNotFoundError:
+            self.error_message = f"File '{pvt_relative_file_path}' not found."
+            return
 
-        self.add_shared_settings_group(Filter(self.pvt_df), PluginIds.SharedSettings.FILTER)
+        except ParserError:
+            self.error_message = f"File '{pvt_relative_file_path}' could not be parsed."
+            return
+
+        except EmptyDataError:
+            self.error_message = f"File '{pvt_relative_file_path}' is an empty file."
+
+        except Exception:
+            self.error_message = (
+                f"'Unknown exception when trying to read {pvt_relative_file_path} '"
+            )
+
+        self.add_store(
+            PluginIds.Stores.SELECTED_PHASE, WebvizPluginABC.StorageType.SESSION
+        )
+        self.add_store(
+            PluginIds.Stores.SELECTED_COLOR, WebvizPluginABC.StorageType.SESSION
+        )
+
+        self.add_shared_settings_group(
+            Filter(self.pvt_df), PluginIds.SharedSettings.FILTER
+        )
 
         self.add_view(
-            PvtView(self.pvt_df),
-            PluginIds.PvtID.INDICATORS,
-            PluginIds.PvtID.GROUP_NAME
+            PvtView(self.pvt_df), PluginIds.PvtID.INDICATORS, PluginIds.PvtID.GROUP_NAME
         )
 
     @property
--- webviz_subsurface/plugins/Pvt_plot/code/_new_pvt_plot/views/_pvt.py
+++ webviz_subsurface/plugins/Pvt_plot/code/_new_pvt_plot/views/_pvt.py
@@ -1,21 +1,33 @@
-
 import pandas as pd
 from webviz_config.webviz_plugin_subclasses import ViewABC
 
+from .._pliginIds import PlugInIds
+from ..view_elements import Graph
+
+
 class PvtView(ViewABC):
     class Ids:
-        #pylint disable too few arguments
-        NAME =""
+        # pylint disable too few arguments
+        FORMATION_VOLUME_FACTOR = "formation-volume-factor"
+        VISCOSITY = "viscosity"
+        DENSITY = "density"
+        GAS_OIL_RATIO = "gas-oil-ratio"
+
     def __init__(self, pvt_df: pd.DataFrame) -> None:
         super().__init__("Pvt View")
 
         self.pvt_df = pvt_df
 
-        
+        column = self.add_column()
 
+        first_row = column.make_row()
+        first_row.add_view_element(Graph(), PopulationIndicators.Ids.POPULATION)
+        first_row.add_view_element(Graph(), PopulationIndicators.Ids.POPULATION_GROWTH)
 
-
-
-
-
-        
+        second_row = column.make_row()
+        second_row.add_view_element(
+            Graph(), PopulationIndicators.Ids.RURAL_VS_URBAN_POPULATION
+        )
+        second_row.add_view_element(
+            Graph(), PopulationIndicators.Ids.RURAL_VS_URBAN_POPULATION_GROWTH
+        )
