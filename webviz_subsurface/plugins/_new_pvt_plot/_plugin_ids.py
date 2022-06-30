1eb648cca01382db043b5af76de718bc0271d7f2
Jincheng Liu (OG SUB RPE) <jliu@be-linrgsn057.be.statoil.no>
2022-06-30 15:04:49 +0200

    Add store IDs

--- webviz_subsurface/plugins/_new_pvt_plot/_plugin_ids.py
+++ webviz_subsurface/plugins/_new_pvt_plot/_plugin_ids.py
@@ -1,7 +1,10 @@
 class PluginIds:
     class Stores:
         SELECTED_COLOR = "color-by"
+        SELECTED_ENSEMBLES = "selected-ensembles"
         SELECTED_PHASE = "selected-phase"
+        SELECTED_PVTNUM = "selected-pvtnum"
+        SELECTED_SHOW_PLOTS = "selected-show-plots"
 
     class SharedSettings:
         FILTER = "filter"
