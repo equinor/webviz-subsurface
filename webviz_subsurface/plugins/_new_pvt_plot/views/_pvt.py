e3299bdc3f88a12cbb95ef25ac65806ef757dbc5
Eirik Sundby Håland (OG SUB RPE) <ehala@be-linrgsn025.be.statoil.no>
2022-06-30 14:00:43 +0200

    fix import in _pvt

--- webviz_subsurface/plugins/_new_pvt_plot/views/_pvt.py
+++ webviz_subsurface/plugins/_new_pvt_plot/views/_pvt.py
@@ -1,17 +1,7 @@
-<<<<<<< HEAD
-
 import pandas as pd
 from webviz_config.webviz_plugin_subclasses import ViewABC
 
-class PvtView(ViewABC):
-    class Ids:
-        #pylint disable too few arguments
-        NAME =""
-=======
-import pandas as pd
-from webviz_config.webviz_plugin_subclasses import ViewABC
-
-from .._plugin_ids import PlugInIds
+from .._plugin_ids import PluginIds
 from ..view_elements import Graph
 
 
@@ -23,22 +13,11 @@ class PvtView(ViewABC):
         DENSITY = "density"
         GAS_OIL_RATIO = "gas-oil-ratio"
 
->>>>>>> origin/PVT_Trail
     def __init__(self, pvt_df: pd.DataFrame) -> None:
         super().__init__("Pvt View")
 
         self.pvt_df = pvt_df
 
-<<<<<<< HEAD
-        
-
-
-
-
-
-
-        
-=======
         column = self.add_column()
 
         first_row = column.make_row()
@@ -48,4 +27,3 @@ class PvtView(ViewABC):
         second_row = column.make_row()
         second_row.add_view_element(Graph(), PvtView.Ids.DENSITY)
         second_row.add_view_element(Graph(), PvtView.Ids.GAS_OIL_RATIO)
->>>>>>> origin/PVT_Trail
