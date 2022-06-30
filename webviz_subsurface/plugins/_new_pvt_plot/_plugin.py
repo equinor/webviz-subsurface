1d36e1320da6722ee4860944e0e77af580c433a4
Eirik Sundby Håland (OG SUB RPE) <ehala@be-linrgsn025.be.statoil.no>
2022-06-30 15:42:12 +0200

    change variable name for dataframe

--- webviz_subsurface/plugins/_new_pvt_plot/_plugin.py
+++ webviz_subsurface/plugins/_new_pvt_plot/_plugin.py
@@ -16,7 +16,7 @@ from ..._datainput.pvt_data import load_pvt_csv, load_pvt_dataframe
 
 
 class PvtPlotter(WebvizPluginABC):
-    
+
     PHASES = ["OIL", "GAS", "WATER"]
 
     def __init__(
@@ -44,27 +44,27 @@ class PvtPlotter(WebvizPluginABC):
         self.drop_ensemble_duplicates = drop_ensemble_duplicates
 
         if self.pvt_relative_file_path is None:
-            self.pvt_data_frame = load_pvt_dataframe(
+            self.pvt_df = load_pvt_dataframe(
                 self.ensemble_paths,
                 use_init_file=read_from_init_file,
                 drop_ensemble_duplicates=drop_ensemble_duplicates,
             )
         else:
             # Load data from all ensembles into a pandas DataFrame
-            self.pvt_data_frame = load_pvt_csv(
+            self.pvt_df = load_pvt_csv(
                 ensemble_paths=self.ensemble_paths,
                 csv_file=self.pvt_relative_file_path,
                 drop_ensemble_duplicates=drop_ensemble_duplicates,
             )
 
-            self.pvt_data_frame = self.pvt_data_frame.rename(
+            self.pvt_df = self.pvt_df.rename(
                 str.upper, axis="columns"
             ).rename(
                 columns={"TYPE": "KEYWORD", "RS": "RATIO", "R": "RATIO", "GOR": "RATIO"}
             )
 
         # Ensure that the identifier string "KEYWORD" is contained in the header columns
-        if "KEYWORD" not in self.pvt_data_frame.columns:
+        if "KEYWORD" not in self.pvt_df.columns:
             raise ValueError(
                 (
                     "There has to be a KEYWORD or TYPE column with corresponding Eclipse keyword."
@@ -73,17 +73,17 @@ class PvtPlotter(WebvizPluginABC):
             )
 
         self.phases_additional_info: List[str] = []
-        if self.pvt_data_frame["KEYWORD"].str.contains("PVTO").any():
+        if self.pvt_df["KEYWORD"].str.contains("PVTO").any():
             self.phases_additional_info.append("PVTO")
-        elif self.pvt_data_frame["KEYWORD"].str.contains("PVDO").any():
+        elif self.pvt_df["KEYWORD"].str.contains("PVDO").any():
             self.phases_additional_info.append("PVDO")
-        elif self.pvt_data_frame["KEYWORD"].str.contains("PVCDO").any():
+        elif self.pvt_df["KEYWORD"].str.contains("PVCDO").any():
             self.phases_additional_info.append("PVCDO")
-        if self.pvt_data_frame["KEYWORD"].str.contains("PVTG").any():
+        if self.pvt_df["KEYWORD"].str.contains("PVTG").any():
             self.phases_additional_info.append("PVTG")
-        elif self.pvt_data_frame["KEYWORD"].str.contains("PVDG").any():
+        elif self.pvt_df["KEYWORD"].str.contains("PVDG").any():
             self.phases_additional_info.append("PVDG")
-        if self.pvt_data_frame["KEYWORD"].str.contains("PVTW").any():
+        if self.pvt_df["KEYWORD"].str.contains("PVTW").any():
             self.phases_additional_info.append("PVTW")
 
 
