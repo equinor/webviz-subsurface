97a43c649f60fbd89a52351551c6ada2615c6f95
Jincheng Liu (OG SUB RPE) <jliu@be-linrgsn057.be.statoil.no>
2022-07-01 10:00:33 +0200

    update _show_settings

--- webviz_subsurface/plugins/_new_pvt_plot/shared_settings/_show_settings.py
+++ webviz_subsurface/plugins/_new_pvt_plot/shared_settings/_show_settings.py
@@ -1,6 +1,6 @@
-from typing import List
+from typing import List, Tuple, Dict
 from dash.development.base_component import Component
-from dash import callback, Input, Output
+from dash import callback, Input, Output, State
 
 import pandas as pd
 from webviz_config.webviz_plugin_subclasses import SettingsGroupABC
@@ -15,24 +15,61 @@ class ShowPlots(SettingsGroupABC):
 
     def __init__(self, pvt_df: pd.DataFrame) -> None:
         super().__init__("Show Plots")
+
         self.plot_settings = ["Formation Volume Factor", "Viscosity", "Density", "Gas/Oil Ratio (Rs)"]
+
+
+    @staticmethod
+    def plot_visibility_options(phase: str = "") -> Dict[str, str]:
+        options = {
+            "fvf": "Formation Volume Factor",
+            "viscosity": "Viscosity",
+            "density": "Density",
+        }
+        if phase == "Water (PVTW)":
+            options["ratio"] = "Gas/Oil Ratio (Rs)"
+        if phase == "Gas (PVTG)":
+            options["ratio"] = "Vaporized Oil Ratio (Rv)"
+        return options
+
 
     def layout(self) -> List[Component]:
         return [
                 wcc.Checklist(
                 id = self.register_component_unique_id(ShowPlots.Ids.SHOWPLOTS),
-                options =[
-                    {"label": x, "value":x} for x in self.plot_settings
-                    ],
-                value =self.plot_settings,
+                options =self.plot_visibility_options(),
+                # [
+                #     {"label": x, "value":x} for x in self.plot_visibility_options().items()
+                #     ],
+                value =["fvf", "viscosity","density","ratio"],
                 vertical= True
                 ),
         ]
 
     def set_callbacks(self) -> None:
         @callback(
-            Output(self.get_store_unique_id(PluginIds.Stores.SELECTED_SHOW_PLOTS).to_string(),'data'),
+            Output(self.get_store_unique_id(PluginIds.Stores.SELECTED_SHOW_PLOTS),'data'),
             Input(self.component_unique_id(ShowPlots.Ids.SHOWPLOTS).to_string(),'value')
         )
         def _update_show_plots(selected_plots: List[str]) -> List[str]:
             return selected_plots
+
+        @callback(
+            [
+                Output(self.component_unique_id(ShowPlots.Ids.SHOWPLOTS).to_string(), "options"),
+                Output(self.component_unique_id(ShowPlots.Ids.SHOWPLOTS).to_string(), "value"),
+            ],
+            Input(self.component_unique_id(Filter.Ids.PHASE).to_string(), "value"),
+            Input(self.component_unique_id(ShowPlots.Ids.SHOWPLOTS).to_string(), "value"),
+        )
+        def _set_available_plots(
+            phase: str,
+            values: List[str],
+        ) -> Tuple[List[dict], List[str]]:
+            visibility_options = self.plot_visibility_options(phase)
+            print(visibility_options)
+            return (
+                [visibility_options],
+                # [{"label": l, "value": v} for v, l in visibility_options.items()],
+                [value for value in values if value in visibility_options],
+            )
