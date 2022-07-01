ef989e3cfa8e2eb48f86b043f66d16222251445a
Eirik Sundby Håland (OG SUB RPE) <ehala@be-linrgsn025.be.statoil.no>
2022-07-01 15:26:22 +0200

    layout function for view

--- webviz_subsurface/plugins/_new_pvt_plot/views/_pvt.py
+++ webviz_subsurface/plugins/_new_pvt_plot/views/_pvt.py
@@ -3,6 +3,7 @@ from typing import List
 from dash import callback, Input, Output
 import pandas as pd
 from webviz_config.webviz_plugin_subclasses import ViewABC
+from webviz_config import WebvizSettings
 
 from .._plugin_ids import PluginIds
 from ..view_elements import Graph
@@ -21,6 +22,7 @@ class PvtView(ViewABC):
         super().__init__("Pvt View")
 
         self.pvt_df = pvt_df
+        self.plotly_theme = WebvizSettings.theme.plotly_theme
 
         column = self.add_column()
 
@@ -56,10 +58,15 @@ class PvtView(ViewABC):
                             colors,
                             phase,
                             "RATIO",
-                            False,<
+                            False,
                             True,
                             True,
                         ),
-                "layout": {"title": "Formation Volume Factor"}
+                "layout": plot_layout(
+                            color_by,
+                            self.plotly_theme,
+                            rf"Pressure [{PVT_df['PRESSURE_UNIT'].iloc[0]}]",
+                            rf"[{PVT_df['VOLUMEFACTOR_UNIT'].iloc[0]}]",
+                        )
             }
             return formation_volume_factor
