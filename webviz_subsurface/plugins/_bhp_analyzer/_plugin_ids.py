66df7c9cd2fa5af25c6b8d999b029ad2c606da2a
Eirik Sundby Håland (OG SUB RPE) <ehala@be-linrgsn025.be.statoil.no>
2022-07-05 15:23:20 +0200

    add pluginIds

---
+++ webviz_subsurface/plugins/_bhp_analyzer/_plugin_ids.py
@@ -0,0 +1,21 @@
+class PluginIds:
+    class Stores:
+        SELECTED_ENSEMBLE = "selected-ensemble"
+        SELECTED_WELLS = "selected-wells"
+        SELECTED_MAX_NUMBER_OF_WELLS = "max-number-of-wells"
+        SELECTED_SORT_BY = "sort-by"
+        SELECTED_ASCENDING_DESCENDING = "ascending-descending"
+
+    class SharedSettings:
+        FILTER = "filter"
+
+    class BhpID:
+        GROUP_NAME = "BHP Analyzer"
+
+        LINE_CHART = "line-chart"
+
+        FAN_CHART = "fan-chart"
+
+        BAR_CHART = "bar-chart"
+
+    
