e27d79d1f053dce1d3f9b04d31aaa64098b9fb24
Eirik Sundby Håland (OG SUB RPE) <ehala@be-linrgsn025.be.statoil.no>
2022-07-07 15:20:52 +0200

    add pluginIds

---
+++ webviz_subsurface/plugins/_parameter_parallel_coordinates/_pluginIds.py
@@ -0,0 +1,22 @@
+class PluginIds:
+    # pylint: disable=too-few-public-methods
+    class Stores:
+        # pylint: disable=too-few-public-methods
+        SELECTED_ENSEMBLE = "selected-ensemble"
+        SELECTED_EXCLUDE_INCLUDE = "selected-exclude-include"
+        SELECTED_PARAMETERS = "selected-parameters"
+        SELECTED_RESPONSE = "slected-response"
+        SELECTED_DATE = "selected-date"
+
+    class SharedSettings:
+        # pylint: disable=too-few-public-methods
+        FILTER = "filter"
+        RESPONSE_SETTINGS = "response-settings"
+
+    class ParallelID:
+        # pylint: disable=too-few-public-methods
+        GROUP_NAME = ""
+
+        ENSEMBLE_CHART = "ensemble-chart"
+
+        PARALLEL_CHART = "parallel-chart"
