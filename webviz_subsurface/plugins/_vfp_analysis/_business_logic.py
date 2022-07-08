298e9bc7a3f14cb6c3d90b753c085c1d994111e2
Øyvind Lind-Johansen <olind@equinor.com>
2022-07-08 15:00:49 +0200

    Filters imports the variable values

--- webviz_subsurface/plugins/_vfp_analysis/_business_logic.py
+++ webviz_subsurface/plugins/_vfp_analysis/_business_logic.py
@@ -1,10 +1,90 @@
 import re
 from pathlib import Path
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 
 import pandas as pd
 from webviz_config import WebvizSettings
 from webviz_config.webviz_store import webvizstore
+
+from ._types import (
+    AlqType,
+    GfrType,
+    PressureType,
+    RateType,
+    TabType,
+    UnitSystem,
+    WfrType,
+)
+
+
+class VfpTable:
+
+    REQUIRED_COLUMNS = [
+        "RATE",
+        "PRESSURE",
+        "WFR",
+        "GFR",
+        "ALQ",
+        "TAB",
+        "VFP_TYPE",
+        "TABLE_NUMBER",
+        "DATUM",
+        "RATE_TYPE",
+        "WFR_TYPE",
+        "GFR_TYPE",
+        "ALQ_TYPE",
+        "PRESSURE_TYPE",
+        "TAB_TYPE",
+        "UNIT_TYPE",
+    ]
+
+    def __init__(self, table_nb: int, table_df: pd.DataFrame) -> None:
+
+        self._table_nb = table_nb
+        # This QCs that the required columns are there
+        self._table_df = table_df[VfpTable.REQUIRED_COLUMNS]
+
+        self._wfr_type = WfrType(self._get_parameter_type("WFR_TYPE"))
+        self._gfr_type = GfrType(self._get_parameter_type("GFR_TYPE"))
+        self._alq_type = AlqType(self._get_parameter_type("ALQ_TYPE"))
+        self._pressure_type = PressureType(self._get_parameter_type("PRESSURE_TYPE"))
+        self._rate_type = RateType(self._get_parameter_type("RATE_TYPE"))
+        self._tab_type = TabType(self._get_parameter_type("TAB_TYPE"))
+        self._unit_type = UnitSystem(self._get_parameter_type("UNIT_TYPE"))
+
+    def get_parameter_data(self) -> Dict[str, Dict[str, Any]]:
+        """Returns the VFP parameters"""
+        return {
+            "WFR": {
+                "type": self._wfr_type,
+                "values": sorted(list(self._table_df["WFR"].unique())),
+            },
+            "GFR": {
+                "type": self._gfr_type,
+                "values": sorted(list(self._table_df["GFR"].unique())),
+            },
+            "ALQ": {
+                "type": self._alq_type,
+                "values": sorted(list(self._table_df["ALQ"].unique())),
+            },
+            "PRESSURE": {
+                "type": self._pressure_type,
+                "values": sorted(list(self._table_df["PRESSURE"].unique())),
+            },
+            "RATE": {
+                "type": self._rate_type,
+                "values": sorted(list(self._table_df["RATE"].unique())),
+            },
+        }
+
+    def _get_parameter_type(self, param_name: str) -> str:
+        if self._table_df[param_name].nunique() > 1:
+            raise ValueError(
+                f"There can only be one {param_name} per table: "
+                f"{self._table_df[param_name].unique()} was found "
+                f"in table {self._table_nb}"
+            )
+        return self._table_df[param_name].values[0]
 
 
 class VfpDataModel:
@@ -35,13 +115,25 @@ class VfpDataModel:
         else:
             self._csvfile = Path(csvfile)
 
-        self._dframe = read_csv(self._csvfile)
+        dframe = read_csv(self._csvfile)
 
-        ## QC that the right columns are there
+        if dframe.empty:
+            raise ValueError(f"No VFP tables found in file: {self._csvfile}")
+
+        self._vfp_tables = {
+            table_nb: VfpTable(table_nb, table_df)
+            for table_nb, table_df in dframe.groupby("TABLE_NUMBER")
+        }
 
     def get_vfp_numbers(self) -> List[int]:
         """Return unique vfp numbers"""
-        return sorted(list(self._dframe["TABLE_NUMBER"].unique()))
+        return sorted(list(self._vfp_tables.keys()))
+
+    def get_vfp_table(self, table_number: int) -> VfpTable:
+        """Returns a VfpTable object corresponding to the given table number"""
+        if not table_number in self._vfp_tables:
+            raise ValueError(f"Vfp Table number {table_number} not found.")
+        return self._vfp_tables[table_number]
 
 
 @webvizstore
--- webviz_subsurface/plugins/_vfp_analysis/views/_vfp_view.py
+++ webviz_subsurface/plugins/_vfp_analysis/views/_vfp_view.py
@@ -1,9 +1,10 @@
-from typing import Any, List
+from typing import Any, Dict, List, Tuple
 
 import webviz_core_components as wcc
+from dash import Input, Output, callback, html
 from webviz_config.webviz_plugin_subclasses import SettingsGroupABC, ViewABC
 
-from .._business_logic import VfpDataModel
+from .._business_logic import VfpDataModel, VfpTable
 from ..view_elements import VfpViewElement
 
 
@@ -30,11 +31,62 @@ class ViewSettings(SettingsGroupABC):
         ]
 
 
+class Filters(SettingsGroupABC):
+    class Ids:
+        # pylint: disable=too-few-public-methods
+        RATE_LABEL = "rate-label"
+        PRESSURE_LABEL = "pressure-label"
+        WFR_LABEL = "wfr-label"
+        GFR_LABEL = "gfr-label"
+        ALQ_LABEL = "alq-label"
+        RATE = "RATE"
+        PRESSURE = "pressure"
+        WFR = "wfr"
+        GFR = "gfr"
+        ALQ = "alq"
+
+    def __init__(self) -> None:
+        super().__init__("Filters")
+
+    def layout(self) -> List[Any]:
+        return [
+            html.Label(
+                id=self.register_component_unique_id(Filters.Ids.RATE_LABEL),
+            ),
+            wcc.SelectWithLabel(
+                id=self.register_component_unique_id(Filters.Ids.RATE),
+            ),
+            html.Label(
+                id=self.register_component_unique_id(Filters.Ids.PRESSURE_LABEL),
+            ),
+            wcc.SelectWithLabel(
+                id=self.register_component_unique_id(Filters.Ids.PRESSURE),
+            ),
+            html.Label(
+                id=self.register_component_unique_id(Filters.Ids.WFR_LABEL),
+            ),
+            wcc.SelectWithLabel(
+                id=self.register_component_unique_id(Filters.Ids.WFR),
+            ),
+            html.Label(
+                id=self.register_component_unique_id(Filters.Ids.GFR_LABEL),
+            ),
+            wcc.SelectWithLabel(id=self.register_component_unique_id(Filters.Ids.GFR)),
+            html.Label(
+                id=self.register_component_unique_id(Filters.Ids.ALQ_LABEL),
+            ),
+            wcc.SelectWithLabel(
+                id=self.register_component_unique_id(Filters.Ids.ALQ),
+            ),
+        ]
+
+
 class VfpView(ViewABC):
     class Ids:
         # pylint: disable=too-few-public-methods
         VIEW_ELEMENT = "view-element"
         SETTINGS = "settings"
+        FILTERS = "filters"
 
     def __init__(self, data_model: VfpDataModel) -> None:
         super().__init__("VFP Analysis")
@@ -44,6 +96,157 @@ class VfpView(ViewABC):
         self.add_settings_group(
             ViewSettings(self._data_model.get_vfp_numbers()), VfpView.Ids.SETTINGS
         )
+        self.add_settings_group(Filters(), VfpView.Ids.FILTERS)
 
         column = self.add_column()
         column.add_view_element(VfpViewElement(), VfpView.Ids.VIEW_ELEMENT)
+
+    def set_callbacks(self) -> None:
+        @callback(
+            # Options
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.RATE)
+                .to_string(),
+                "options",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.PRESSURE)
+                .to_string(),
+                "options",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.WFR)
+                .to_string(),
+                "options",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.GFR)
+                .to_string(),
+                "options",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.ALQ)
+                .to_string(),
+                "options",
+            ),
+            # Values
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.RATE)
+                .to_string(),
+                "value",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.PRESSURE)
+                .to_string(),
+                "value",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.WFR)
+                .to_string(),
+                "value",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.GFR)
+                .to_string(),
+                "value",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.ALQ)
+                .to_string(),
+                "value",
+            ),
+            # Labels
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.RATE_LABEL)
+                .to_string(),
+                "children",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.PRESSURE_LABEL)
+                .to_string(),
+                "children",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.WFR_LABEL)
+                .to_string(),
+                "children",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.GFR_LABEL)
+                .to_string(),
+                "children",
+            ),
+            Output(
+                self.settings_group(self.Ids.FILTERS)
+                .component_unique_id(Filters.Ids.ALQ_LABEL)
+                .to_string(),
+                "children",
+            ),
+            # Input
+            Input(
+                self.settings_group(self.Ids.SETTINGS)
+                .component_unique_id(ViewSettings.Ids.VFP_NUMBER)
+                .to_string(),
+                "value",
+            ),
+        )
+        def _update_filters(
+            vfp_number: int,
+        ) -> Tuple[
+            List[Dict[str, float]],
+            List[Dict[str, float]],
+            List[Dict[str, float]],
+            List[Dict[str, float]],
+            List[Dict[str, float]],
+            List[float],
+            List[float],
+            List[float],
+            List[float],
+            List[float],
+            str,
+            str,
+            str,
+            str,
+            str,
+        ]:
+
+            vfp_table: VfpTable = self._data_model.get_vfp_table(vfp_number)
+            param_data = vfp_table.get_parameter_data()
+
+            rate_values = param_data["RATE"]["values"]
+            pressure_values = param_data["PRESSURE"]["values"]
+            wfr_values = param_data["WFR"]["values"]
+            gfr_values = param_data["GFR"]["values"]
+            alq_values = param_data["ALQ"]["values"]
+
+            return (
+                [{"label": value, "value": value} for value in rate_values],
+                [{"label": value, "value": value} for value in pressure_values],
+                [{"label": value, "value": value} for value in wfr_values],
+                [{"label": value, "value": value} for value in gfr_values],
+                [{"label": value, "value": value} for value in alq_values],
+                rate_values,
+                pressure_values,
+                wfr_values,
+                gfr_values,
+                alq_values,
+                param_data["RATE"]["type"].name,
+                param_data["PRESSURE"]["type"].name,
+                param_data["WFR"]["type"].name,
+                param_data["GFR"]["type"].name,
+                param_data["ALQ"]["type"].name,
+            )
