506a8182bf3b3436f51a2b4eab2150c2ae339cfc
Viktoria Christine Vahlin (OG SUB RPE) <vvah@be-linrgsn154.be.statoil.no>
2022-07-14 12:09:06 +0200

    fixed try except

--- webviz_subsurface/plugins/_parameter_correlation/views/_parameter_plot.py
+++ webviz_subsurface/plugins/_parameter_correlation/views/_parameter_plot.py
@@ -36,8 +36,8 @@ class ParameterPlot(ViewABC):
         self.p_cols = p_cols
         try:
             self.plotly_theme = webviz_settings.theme.plotly_theme
-        except AttributeError:
-            raise AttributeError("'Dash' has no attribute 'theme'")
+        except AttributeError as exc:
+            raise AttributeError("'Dash' has no attribute 'theme'") from exc
         self.drop_constants = drop_constants
 
         column = self.add_column()
