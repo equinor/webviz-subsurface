226b33062edcf35eb5f2aaf4b1ab8fe0c536e4d8
Viktoria Vahlin <107865041+vvahlin@users.noreply.github.com>
2022-07-15 08:17:56 +0200

    Edit spelling error

--- tests/integration_tests/test_parameter_corr.py
+++ tests/integration_tests/test_parameter_corr.py
@@ -38,7 +38,7 @@ def test_parameter_corr(dash_duo: dash.testing.composite.DashComposite) -> None:
         my_component = dash_duo.find_element(
             f"""#
             {parameter_correlation.shared_settings_group(
-                    parameter_correlationPlugInIDs.SharedSettings.BOTHPLOTS
+                    parameter_correlation.PlugInIDs.SharedSettings.BOTHPLOTS
                 )
                 .component_unique_id(
                     parameter_correlation.BothPlots.IDs.ENSEMBLE
