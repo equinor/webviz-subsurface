be8734d8674ed58e1b4f9259e4ff66d8b7f88645
Eirik Sundby Håland (OG SUB RPE) <ehala@be-linrgsn089.be.statoil.no>
2022-07-25 14:12:19 +0200

    reformating

--- webviz_subsurface/plugins/_well_analysis/_error.py
+++ webviz_subsurface/plugins/_well_analysis/_error.py
@@ -2,5 +2,4 @@ from dash import html
 
 
 def error(error_message: str) -> html.Div:
-    return html.Div(children=error_message, style={"color": "red"} )
-
+    return html.Div(children=error_message, style={"color": "red"})
