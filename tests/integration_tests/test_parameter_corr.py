15732a055b752ffb06768cc03e839b8f983c2a5e
Viktoria Vahlin <107865041+vvahlin@users.noreply.github.com>
2022-07-25 08:42:20 +0200

    Remove #

--- tests/integration_tests/test_parameter_corr.py
+++ tests/integration_tests/test_parameter_corr.py
@@ -38,7 +38,7 @@ def test_parameter_corr(dash_duo: dash.testing.composite.DashComposite) -> None:
         # Using str literals directly, not IDs from the plugin as intended because
         # the run test did not accept the imports
         my_component = dash_duo.find_element(
-            f"#{parameter_correlation.shared_settings_group("both-plots")"
+            f"{parameter_correlation.shared_settings_group("both-plots")"
             f".component_unique_id("ensemble").to_string()}"
         )
 
