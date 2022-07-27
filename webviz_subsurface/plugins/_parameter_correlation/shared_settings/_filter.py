3a3d987d93fa3fdf76dc742fc7219646f3f03357
Viktoria Christine Vahlin (OG SUB RPE) <vvah@be-linrgsn154.be.statoil.no>
2022-07-27 08:46:01 +0200

    edit id

--- webviz_subsurface/plugins/_parameter_correlation/shared_settings/_filter.py
+++ webviz_subsurface/plugins/_parameter_correlation/shared_settings/_filter.py
@@ -14,7 +14,7 @@ class BothPlots(SettingsGroupABC):
 
     class IDs:
         # pylint: disable=too-few-public-methods
-        ENSEMBLE = "ensemble-all"
+        ENSEMBLE = "ensemble"
 
     def __init__(self, ensembles: dict) -> None:
         super().__init__("Ensemble in both plots")
