926729fc4722046b86da3395c95140afef9feb35
Jincheng Liu (OG SUB RPE) <jliu@be-linrgsn012.be.statoil.no>
2022-08-02 14:15:48 +0200

    reformatting [deploy test]
    se enter the commit message for your changes. Lines starting

--- webviz_subsurface/plugins/_seismic_misfit/_views/_crossplot.py
+++ webviz_subsurface/plugins/_seismic_misfit/_views/_crossplot.py
@@ -1,4 +1,4 @@
-from typing import Dict, List, Optional, Tuple, Union
+from typing import Dict, List, Optional, Union
 
 import webviz_core_components as wcc
 from dash import Input, Output, callback, dcc
