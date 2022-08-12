acec082c52538ba1a72de6b2b3f0d382d4656464
jorgenherje <jorgen.herje@ceetronsolutions.com>
2022-08-12 15:32:49 +0200

    Remove unused store Id definitions

--- webviz_subsurface/plugins/_simulation_time_series/_plugin_ids.py
+++ webviz_subsurface/plugins/_simulation_time_series/_plugin_ids.py
@@ -1,22 +1,6 @@
 class PluginIds:
 
     # pylint: disable=too-few-public-methods
-    # class Stores:
-    # SUBPLOT_OWNER_OPTIONS_RADIO_ITEMS = "subplot_owner_options_radio_items"
-    # ENSEMBLES_DROPDOWN = "ensembles_dropdown"
-    # RELATIVE_DATE_DROPDOWN = "relative_date_dropdown"
-    # RESAMPLING_FREQUENCY_DROPDOWN = "resampling_frequency_dropdown"
-    # CREATED_DELTA_ENSEMBLES = "created_delta_ensemble_names"
-    # ENSEMBLES_DROPDOWN_OPTIONS = "ensembles_dropdown-options"
-    # VECTOR_CALCULATOR_EXPRESSIONS = "vector_calculator_expressions"
-    # VISUALIZATION_RADIO_ITEMS = "visualization_radio_items"
-    # PLOT_FANCHART_OPTIONS_CHECKLIST = "plot_fanchart_options_checklist"
-    # PLOT_STATISTICS_OPTIONS_CHECKLIST = "plot_statistics_options_checklist"
-    # PLOT_TRACE_OPTIONS_CHECKLIST = "plot_trace_options_checklist"
-    # REALIZATIONS_FILTER_SELECTOR = "realizations_filter_selector"
-    # STATISTICS_FROM_RADIO_ITEMS = "statistics_from_radio_items"
-
-    # pylint: disable=too-few-public-methods
     class SharedSettings:
         GROUP_BY = "group_by"
         RESAMPLING_FREQUENCY = "resampling-frequency"
