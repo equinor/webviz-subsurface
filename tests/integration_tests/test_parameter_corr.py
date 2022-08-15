b6463991f0f5f5017a0d97782eedbfeb802f596e
Hans Kallekleiv <HansKallekleiv@users.noreply.github.com>
2022-08-15 11:17:20 +0200

    Attempt to add a wlf test fixture

--- tests/integration_tests/test_parameter_corr.py
+++ tests/integration_tests/test_parameter_corr.py
@@ -17,13 +17,8 @@ from webviz_config.themes import default_theme
 GET_PARAMETERS = "webviz_subsurface.plugins._parameter_correlation.get_parameters"
 
 
-def test_parameter_corr(dash_duo: dash.testing.composite.DashComposite) -> None:
+def test_parameter_corr(webviz_duo: dash.testing.composite.DashComposite) -> None:
 
-    app = dash.Dash(__name__)
-    app.css.config.serve_locally = True
-    app.scripts.config.serve_locally = True
-    app.config.suppress_callback_exceptions = True
-    CACHE.init_app(app.server)
     webviz_settings = WebvizSettings(
         shared_settings={"scratch_ensembles": {"iter-0": ""}}, theme=default_theme
     )
@@ -34,36 +29,15 @@ def test_parameter_corr(dash_duo: dash.testing.composite.DashComposite) -> None:
 
         parameter_correlation = ParameterCorrelation(webviz_settings, ensembles)
 
-        app.layout = dash.html.Div(
-            className="layoutWrapper",
-            children=[
-                wcc.WebvizContentManager(
-                    id="webviz-content-manager",
-                    children=[
-                        wcc.WebvizSettingsDrawer(
-                            id="settings-drawer",
-                            children=parameter_correlation.get_all_settings(),
-                        ),
-                        wcc.WebvizPluginsWrapper(
-                            id="plugins-wrapper",
-                            children=parameter_correlation.plugin_layout(),
-                        ),
-                    ],
-                ),
-            ],
-        )
-        dash_duo.start_server(app)
+        webviz_duo.start_server(parameter_correlation)
 
-        dash_duo.find_element(".WebvizSettingsDrawer__ToggleOpen").click()
+        webviz_duo.toggle_webviz_drawer()
 
         # Using str literals directly, not IDs from the plugin as intended because
         # the run test did not accept the imports
-        my_component = dash_duo.find_element(
-            "#"
-            + parameter_correlation.shared_settings_group("both-plots")
-            .component_unique_id("ensemble-both")
-            .to_string()
-        )
 
+        my_component = webviz_duo.find_view_setting_group_unique_id(
+            "paracorr", "settings", "shared-ensemble"
+        )
         if not my_component.text.startswith("iter-0"):
             raise AssertionError()
