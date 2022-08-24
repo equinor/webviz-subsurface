c66139b3267e25d18f10ab7a791bafa8125f4db7
Vegard Berg Kvernelv <vegard@nr.no>
2022-08-24 14:30:12 +0200

    Add colormaps based on MPL

--- webviz_subsurface/plugins/_co2_migration/layout.py
+++ webviz_subsurface/plugins/_co2_migration/layout.py
@@ -126,7 +126,7 @@ def main_layout(get_uuid: Callable, ensembles: List[str]) -> html.Div:
                                         coords={"visible": True},
                                         scale={"visible": True},
                                         coordinateUnit="m",
-                                        colorTables=default_color_tables,
+                                        colorTables=_color_tables(),
                                         zoom=-7,
                                     ),
                                 ],
@@ -206,8 +206,8 @@ class MapSelectorLayout(wcc.Selectors):
                         "Color Scale",
                         dcc.Dropdown(
                             id=get_uuid(LayoutElements.COLORMAP_INPUT),
-                            options=[d["name"] for d in default_color_tables],
-                            value=default_color_tables[0]["name"],
+                            options=[d["name"] for d in _color_tables()],
+                            value=_color_tables()[0]["name"],
                         ),
                         "Minimum",
                         html.Div(
@@ -405,3 +405,66 @@ def _compile_property_options():
         {"label": MapAttribute.MAX_AMFG.value, "value": MapAttribute.MAX_AMFG.value},
         {"label": MapAttribute.AMFG_PLUME.value, "value": MapAttribute.AMFG_PLUME.value},
     ]
+
+
+def _color_tables():
+    # Source: https://waldyrious.net/viridis-palette-generator/
+    # + matplotlib._cm_listed
+    return default_color_tables + [
+        {
+            "name": "Viridis",
+            "discrete": False,
+            "colors": [
+                [0.0, 253, 231, 37],
+                [0.25, 94, 201, 98],
+                [0.50, 33, 145, 140],
+                [0.75, 59, 82, 139],
+                [1.0, 68, 1, 84],
+            ],
+        },
+        {
+            "name": "Inferno",
+            "discrete": False,
+            "colors": [
+                [0.0, 252, 255, 164],
+                [0.25, 249, 142, 9],
+                [0.5, 188, 55, 84],
+                [0.75, 87, 16, 110],
+                [1.0, 0, 0, 4],
+            ],
+        },
+        {
+            "name": "Magma",
+            "discrete": False,
+            "colors": [
+                [0.0, 252, 253, 191],
+                [0.25, 252, 137, 97],
+                [0.5, 183, 55, 121],
+                [0.75, 81, 18, 124],
+                [1.0, 0, 0, 4],
+            ],
+        },
+        {
+            "name": "Plasma",
+            "discrete": False,
+            "colors": [
+                [0.0, 240, 249, 33],
+                [0.25, 248, 149, 64],
+                [0.5, 204, 71, 120],
+                [0.75, 126, 3, 168],
+                [1.0, 13, 8, 135],
+            ],
+        },
+        {
+            "name": "Cividis",
+            "discrete": False,
+            "colors": [
+                 [0.0, 0, 32, 77],
+                 [0.25, 64, 77, 107],
+                 [0.5, 124, 123, 120],
+                 [0.75, 188, 175, 111],
+                 [1.0, 255, 234, 70],
+            ],
+        },
+    ]
+
