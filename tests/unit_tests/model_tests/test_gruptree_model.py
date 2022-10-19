dd31a47d4687ec963d4dfb1000f804f2569d4ab8
Øyvind Lind-Johansen <olind@equinor.com>
2022-10-19 12:50:59 +0200

    Improved tests with fixture creating the gruptree model

--- tests/unit_tests/model_tests/test_gruptree_model.py
+++ tests/unit_tests/model_tests/test_gruptree_model.py
@@ -1,3 +1,4 @@
+import datetime
 from pathlib import Path
 
 import pandas as pd
@@ -6,23 +7,58 @@ from pandas._testing import assert_frame_equal
 
 from webviz_subsurface._models.gruptree_model import GruptreeModel
 
+CHECK_COLUMNS = ["DATE", "CHILD", "KEYWORD", "PARENT"]
+ENSEMBLE = "01_drogon_ahm"
+GRUPTREE_FILE = "share/results/tables/gruptree.csv"
+
+
+@pytest.fixture
+def fixture_model(testdata_folder) -> GruptreeModel:
+    ens_path = Path(testdata_folder) / ENSEMBLE / "realization-*" / "iter-0"
+    return GruptreeModel(
+        ens_name="iter-0", ens_path=ens_path, gruptree_file=GRUPTREE_FILE
+    )
+
 
 @pytest.mark.usefixtures("app")
-def test_gruptree_model(testdata_folder):
+def test_gruptree_model_init(testdata_folder):
 
-    ens_path = Path(testdata_folder) / "01_drogon_ahm" / "realization-*" / "iter-0"
-    gruptree_file = "share/results/tables/gruptree.csv"
-
-    model = GruptreeModel(
-        ens_name="iter-0", ens_path=ens_path, gruptree_file=gruptree_file
-    )
+    assert len(fixture_model.dataframe["REAL"]) == 1
 
     # Load gruptree table from realization-0 and compare with
     # the dataframe from the gruptree_model
-    r0_path = str(ens_path).replace("*", "0")
-    exp_dframe = pd.read_csv(f"{r0_path}/{gruptree_file}")
-    exp_dframe["DATE"] = pd.to_datetime(exp_dframe["DATE"])
-    exp_dframe = exp_dframe.where(pd.notnull(exp_dframe), None)
-    check_columns = ["DATE", "CHILD", "KEYWORD", "PARENT"]
+    r0_path = f"{testdata_folder}/{ENSEMBLE}/realization-0/iter-0/{GRUPTREE_FILE}"
+    exp_df = pd.read_csv(r0_path)
+    exp_df["DATE"] = pd.to_datetime(exp_df["DATE"])
+    exp_df = exp_df.where(pd.notnull(exp_df), None)
 
-    assert_frame_equal(model.dataframe[check_columns], exp_dframe[check_columns])
+    assert_frame_equal(fixture_model.dataframe[CHECK_COLUMNS], exp_df[CHECK_COLUMNS])
+
+
+def test_get_filtered_dataframe():
+
+    # Test the get_filtered_dataframe function with terminal node different than FIELD
+    filtered_df = fixture_model.get_filtered_dataframe(terminal_node="OP")
+    filtered_df = filtered_df[
+        filtered_df["DATE"] == filtered_df["DATE"].max()
+    ].reset_index()
+    exp_filtered_df = pd.DataFrame(
+        columns=["DATE", "CHILD", "KEYWORD", "PARENT"],
+        data=[
+            [datetime.datetime(year=2018, month=11, day=17), "OP", "GRUPTREE", "FIELD"],
+            [datetime.datetime(year=2018, month=11, day=17), "A1", "WELSPECS", "OP"],
+            [datetime.datetime(year=2018, month=11, day=17), "A2", "WELSPECS", "OP"],
+            [datetime.datetime(year=2018, month=11, day=17), "A3", "WELSPECS", "OP"],
+            [datetime.datetime(year=2018, month=11, day=17), "A4", "WELSPECS", "OP"],
+        ],
+    )
+    assert_frame_equal(filtered_df[CHECK_COLUMNS], exp_filtered_df)
+
+    # Test excl_wells_startswith and excl_wells_endswith
+    assert set(
+        fixture_model.get_filtered_dataframe(
+            terminal_node="FIELD",
+            excl_well_startswith=["R_"],
+            excl_well_endswith=["3", "5"],
+        )["CHILD"].unique()
+    ) == {"FIELD", "OP", "RFT", "WI", "A1", "A2", "A4", "A6"}
