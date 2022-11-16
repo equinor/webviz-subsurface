10d0b287fa759b68d366764d95e70a86425c6ebc
Øyvind Lind-Johansen <olind@equinor.com>
2022-11-16 10:46:33 +0100

    Small fix to test

--- tests/integration_tests/plugin_tests/test_vfp_analysis.py
+++ tests/integration_tests/plugin_tests/test_vfp_analysis.py
@@ -3,7 +3,7 @@ from webviz_config.plugins import VfpAnalysis
 from webviz_config.testing import WebvizComposite
 
 
-def test_vfp_analysis(_webviz_duo: WebvizComposite) -> None:
+def test_vfp_analysis(_webviz_duo: WebvizComposite, shared_settings: dict) -> None:
     plugin = VfpAnalysis(vfp_file_pattern="../../data/vfp.arrow")
 
     _webviz_duo.start_server(plugin)
