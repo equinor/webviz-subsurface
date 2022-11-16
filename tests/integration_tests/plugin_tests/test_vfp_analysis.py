be195b544b397e53b6405481f8a690e4aeb4f1e9
Øyvind Lind-Johansen <olind@equinor.com>
2022-11-16 11:55:41 +0100

    changed relative path

--- tests/integration_tests/plugin_tests/test_vfp_analysis.py
+++ tests/integration_tests/plugin_tests/test_vfp_analysis.py
@@ -5,7 +5,7 @@ from webviz_config.testing import WebvizComposite
 
 def test_vfp_analysis(_webviz_duo: WebvizComposite, shared_settings: dict) -> None:
     plugin = VfpAnalysis(
-        shared_settings["HM_SETTINGS"], vfp_file_pattern="../../data/vfp.arrow"
+        shared_settings["HM_SETTINGS"], vfp_file_pattern="tests/data/vfp.arrow"
     )
 
     _webviz_duo.start_server(plugin)
