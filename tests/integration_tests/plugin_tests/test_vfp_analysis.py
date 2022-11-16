dd05407cceb6bab5f88aa869e47560108f8b0d1e
Øyvind Lind-Johansen <olind@equinor.com>
2022-11-16 10:09:25 +0100

    Testdata and test of VfpAnalysis

---
+++ tests/integration_tests/plugin_tests/test_vfp_analysis.py
@@ -0,0 +1,11 @@
+# pylint: disable=no-name-in-module
+from webviz_config.plugins import VfpAnalysis
+from webviz_config.testing import WebvizComposite
+
+
+def test_vfp_analysis(_webviz_duo: WebvizComposite) -> None:
+    plugin = VfpAnalysis(vfp_file_pattern="../../data/vfp.arrow")
+
+    _webviz_duo.start_server(plugin)
+
+    assert _webviz_duo.get_logs() == []
