d37ee91e366fec9b62788f70fefff9349785decb
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2023-04-12 14:51:53 +0200

    Prepare release (#1208)

--- setup.py
+++ setup.py
@@ -108,7 +108,7 @@ setup(
         "vtk>=9.2.2",
         "webviz-config",
         "webviz-core-components>=0.6",
-        "webviz-subsurface-components>=0.4.14",
+        "webviz-subsurface-components>=0.4.15",
     ],
     extras_require={"tests": TESTS_REQUIRE},
     setup_requires=["setuptools_scm~=3.2"],
