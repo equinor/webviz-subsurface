3d63091aaa895adcc0d556b0e0a1bb3224f8f140
Øyvind Lind-Johansen <olind@equinor.com>
2023-05-15 12:00:29 +0200

    Added test

---
+++ tests/integration_tests/plugin_tests/test_simulation_timeseries_onebyone.py
@@ -0,0 +1,23 @@
+import warnings
+
+# pylint: disable=no-name-in-module
+from webviz_config.plugins import SimulationTimeSeriesOneByOne
+from webviz_config.testing import WebvizComposite
+
+
+def test_simulation_timeseries_onebyone(
+    _webviz_duo: WebvizComposite, shared_settings: dict
+) -> None:
+    plugin = SimulationTimeSeriesOneByOne(
+        webviz_settings=shared_settings["SENS_SETTINGS"],
+        ensembles=shared_settings["SENS_ENSEMBLES"],
+        initial_vector="FOPT",
+    )
+    _webviz_duo.start_server(plugin)
+    logs = []
+    for log in _webviz_duo.get_logs():
+        if "dash_renderer" in log.get("message"):
+            warnings.warn(log.get("message"))
+        else:
+            logs.append(log)
+    assert not logs
