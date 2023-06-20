3e5bf2011cf6417b1755032208f0e46deecd891e
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2023-06-20 12:19:04 +0200

    Fix permissions for surface array server cache

--- webviz_subsurface/_providers/ensemble_surface_provider/surface_array_server.py
+++ webviz_subsurface/_providers/ensemble_surface_provider/surface_array_server.py
@@ -61,6 +61,7 @@ class SurfaceArrayServer:
                 "CACHE_TYPE": "FileSystemCache",
                 "CACHE_DIR": cache_dir,
                 "CACHE_DEFAULT_TIMEOUT": 0,
+                "CACHE_OPTIONS": {"mode": 0o660},
             }
         )
         self._array_cache.init_app(app.server)
