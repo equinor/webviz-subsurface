22be94563fd613bf8f4acd8a937119f2ad273d1a
Therese Natterøy <61694854+tnatt@users.noreply.github.com>
2023-07-17 15:15:51 +0200

    Add filter linking in SimulationTimeseriesOneByOne

--- webviz_subsurface/plugins/_simulation_time_series_onebyone/_views/_onebyone_view/_settings/_sensitivity_filter.py
+++ webviz_subsurface/plugins/_simulation_time_series_onebyone/_views/_onebyone_view/_settings/_sensitivity_filter.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import webviz_core_components as wcc
+from dash import html
 from dash.development.base_component import Component
 from webviz_config.utils import StrEnum
 from webviz_config.webviz_plugin_subclasses import SettingsGroupABC
@@ -9,6 +10,7 @@ from webviz_config.webviz_plugin_subclasses import SettingsGroupABC
 class SensitivityFilter(SettingsGroupABC):
     class Ids(StrEnum):
         SENSITIVITY_FILTER = "sensitivity-filter"
+        SENSITIVITY_FILTER_LINK = "sensitivity-filter-link"
 
     def __init__(self, sensitivities: List[str]) -> None:
         super().__init__("Sensitivity Filter")
@@ -16,10 +18,27 @@ class SensitivityFilter(SettingsGroupABC):
 
     def layout(self) -> List[Component]:
         return [
+            html.Div(
+                style={"margin-bottom": "10px"},
+                children=[
+                    wcc.Checklist(
+                        id=self.register_component_unique_id(
+                            self.Ids.SENSITIVITY_FILTER_LINK
+                        ),
+                        options=[
+                            {
+                                "label": "Apply filter only on timeseries",
+                                "value": "no link",
+                            }
+                        ],
+                        value=[],
+                    ),
+                ],
+            ),
             wcc.SelectWithLabel(
                 id=self.register_component_unique_id(self.Ids.SENSITIVITY_FILTER),
                 options=[{"label": i, "value": i} for i in self._sensitivities],
                 value=self._sensitivities,
                 size=min(20, len(self._sensitivities)),
-            )
+            ),
         ]
