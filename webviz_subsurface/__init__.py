6f05e7baf1e887a36aeb857f620599e316a642d7
Hans Kallekleiv <16436291+HansKallekleiv@users.noreply.github.com>
2026-03-10 15:58:12 +0100

    isort

--- webviz_subsurface/__init__.py
+++ webviz_subsurface/__init__.py
@@ -1,11 +1,11 @@
 import glob
 import pathlib
+from importlib.metadata import PackageNotFoundError, version
 from typing import Dict, Optional
 
 import jsonschema
 import webviz_config
 import yaml
-from importlib.metadata import PackageNotFoundError, version
 
 from webviz_subsurface._utils.user_defined_vector_definitions import (
     USER_DEFINED_VECTOR_DEFINITIONS_JSON_SCHEMA,
